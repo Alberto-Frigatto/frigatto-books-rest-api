2237dcc672378c2875abdf30aa256488473fd64d
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-08 13:29:15 -0300

    feat: Adicionar erros relacionados aos tipos de livros no pacote errors
    Cria o arquivo book_kinds_errors.py.py no pacote errors e adiciona o dicionário com erros relacionados aos tipos de livros.

---
+++ errors/book_kinds_errors.py
@@ -0,0 +1,18 @@
+errors = {
+    'BookKindAlreadyExists': {
+        'message': 'Esse tipo de livro já existe',
+        'status': 409,
+    },
+    'BookKindDoesntExists': {
+        'message': 'Esse tipo de livro não existe',
+        'status': 404,
+    },
+    'InvalidKindBookName': {
+        'message': 'Nome do tipo de livro inválido',
+        'status': 400,
+    },
+    'NoDataSent': {
+        'message': 'Não foi enviado nenhum dado',
+        'status': 400,
+    },
+}
