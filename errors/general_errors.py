a4423220e1bf0f043768bc31f2c0ea93a2a43f0f
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-08 13:22:45 -0300

    feat: Adicionar erros genéricos no pacote errors
    Cria o arquivo general_errors.py no pacote errors e adiciona o dicionário com erros genéricos.

---
+++ errors/general_errors.py
@@ -0,0 +1,14 @@
+errors = {
+    'DatabaseConnection': {
+        'message': 'Erro de conexão no banco de dados',
+        'status': 500,
+    },
+    'MethodNotAllowed': {
+        'message': 'Método HTTP não permitido',
+        'status': 405,
+    },
+    'InvalidContentType': {
+        'message': 'Cabeçalho Content-Type inválido. Deve ser application/json',
+        'status': 415,
+    },
+}
