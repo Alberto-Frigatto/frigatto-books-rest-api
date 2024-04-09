913503316e99343c7f570aaec8c767b3f6976480
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-09 19:38:57 -0300

    refactor: Otimizar classe BookKind
    - Modifica o atributo id para receber o tipo int_pk
    - adiciona o método .strip() no parâmetro kind do método __init__
    - Altera a exceção lançada no método -
    - validate_kind_name
    - corrige o método _is_kind_name_valid

--- models/book_kinds_model.py
+++ models/book_kinds_model.py
@@ -4,27 +4,28 @@ from typing import Any
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from db import db
+from db import db, int_pk
 from handle_errors import CustomError
 
 
 class BookKind(db.Model):
     __tablename__ = 'book_kinds'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int_pk]
     kind: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
 
     def __init__(self, kind: str) -> None:
         self._validate_kind_name(kind)
-        self.kind = kind
+        self.kind = kind.strip().lower()
 
     def _validate_kind_name(self, kind: Any) -> None:
         if not self._is_kind_name_valid(kind):
-            raise CustomError('InvalidKindBookName')
+            raise CustomError('InvalidDataSent')
 
     def _is_kind_name_valid(self, kind: Any) -> bool:
         return (
             isinstance(kind, str)
+            and kind
             and len(kind) <= 30
             and re.match(r'^[a-zA-ZáàãâäéèẽêëíìîĩïóòõôöúùũûüÁÀÃÂÄÉÈẼÊËÍÌÎĨÏÓÒÕÔÖÚÙŨÛÜ\s]+$', kind)
         )
