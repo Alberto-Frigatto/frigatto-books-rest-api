82efbfd908e25fdcdbac8e006d41eb6a8395bd41
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-09 19:21:41 -0300

    feat: Criar classe BaseResource em api.py
    Cria a class BaseResource no arquivo api.py.
    
    Essa classe herda de Resource do flask-restful e é usada como base para as classes view fornecendo uma implementação padrão para o método options.

--- api.py
+++ api.py
@@ -1,3 +1,11 @@
-from flask_restful import Api
+from flask import Response
+from flask_restful import Api, Resource
+
+from response import ResponseSuccess
 
 api = Api()
+
+
+class BaseResource(Resource):
+    def options(self, *args, **kwargs) -> Response:
+        return ResponseSuccess().json()
