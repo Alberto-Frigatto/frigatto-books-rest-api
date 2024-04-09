f8122198dd63360d72c9d1fc795b2d638cff492e
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-09 19:42:18 -0300

    refactor: Remover erros de book_kinds_errors.py
    Remove os erros:
    
    - InvalidKindBookName
    - NoDataSent

--- errors/book_kinds_errors.py
+++ errors/book_kinds_errors.py
@@ -7,12 +7,4 @@ errors = {
         'message': 'Esse tipo de livro não existe',
         'status': 404,
     },
-    'InvalidKindBookName': {
-        'message': 'Nome do tipo de livro inválido',
-        'status': 400,
-    },
-    'NoDataSent': {
-        'message': 'Não foi enviado nenhum dado',
-        'status': 400,
-    },
 }
