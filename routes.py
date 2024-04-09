797c6114abc27e0522d44ccd85104dac12512f00
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-09 19:34:25 -0300

    feat: Adicionar blueprint users_bp no app flask

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
-from views import book_kinds_bp
+from views import book_kinds_bp, users_bp
 
 
 def add_routes(app: Flask):
     app.register_blueprint(book_kinds_bp, url_prefix='/bookKinds')
+    app.register_blueprint(users_bp, url_prefix='/users')
