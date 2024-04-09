7c2c4b047ac76c9237bd6db040ed0cce7beca969
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-09 19:28:28 -0300

    feat: Adicionar erros relacionados aos usuários
    Cria o arquivo users_errors.py no pacote errors.

---
+++ errors/users_errors.py
@@ -0,0 +1,14 @@
+errors = {
+    'UserAlreadyExists': {
+        'message': 'Esse usuário já existe',
+        'status': 409,
+    },
+    'InvalidLogin': {
+        'message': 'username ou password inválidos',
+        'status': 401,
+    },
+    'UserAlreadyAuthenticated': {
+        'message': 'Usuário já logado',
+        'status': 400,
+    },
+}
