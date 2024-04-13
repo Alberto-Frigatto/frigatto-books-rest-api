d044327eddcdff774096f1a269e06649b90c6703
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-13 13:38:48 -0300

    refactor: Refatorar classe BookKind
    Cria os métodos _format_kind_name e update_kind.

--- models/book_kinds_model.py
+++ models/book_kinds_model.py
@@ -16,7 +16,7 @@ class BookKind(db.Model):
 
     def __init__(self, kind: str) -> None:
         self._validate_kind_name(kind)
-        self.kind = kind.strip().lower()
+        self.kind = self._format_kind_name(kind)
 
     def _validate_kind_name(self, kind: Any) -> None:
         if not self._is_kind_name_valid(kind):
@@ -29,3 +29,11 @@ class BookKind(db.Model):
             and len(kind) <= 30
             and re.match(r'^[a-zA-ZáàãâäéèẽêëíìîĩïóòõôöúùũûüÁÀÃÂÄÉÈẼÊËÍÌÎĨÏÓÒÕÔÖÚÙŨÛÜç\s]+$', kind)
         )
+
+    def _format_kind_name(self, kind: str) -> str:
+        return kind.strip().lower()
+
+    def update_kind(self, kind: str) -> None:
+        self._validate_kind_name(kind)
+
+        self.kind = self._format_kind_name(kind)
