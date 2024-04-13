87647c213767749d31e2026bf7c1080023e160b9
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-13 14:18:51 -0300

    feat: Adicionar blueprint book_genres_bp no app flask

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
-from views import book_kinds_bp, users_bp
+from views import book_genres_bp, book_kinds_bp, users_bp
 
 
 def add_routes(app: Flask):
     app.register_blueprint(book_kinds_bp, url_prefix='/bookKinds')
     app.register_blueprint(users_bp, url_prefix='/users')
+    app.register_blueprint(book_genres_bp, url_prefix='/bookGenres')
