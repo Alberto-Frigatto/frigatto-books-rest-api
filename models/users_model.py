624d69b71d1534e8b6c2174b2a386317e65e8c4f
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-13 13:50:28 -0300

    feat: Adicionar funcionalidades à classe User
    Adicionar atributo img_url e métodos para alterar os atributos.

--- models/users_model.py
+++ models/users_model.py
@@ -15,28 +15,43 @@ class User(db.Model):
     id: Mapped[int_pk]
     username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
 
-    def __init__(self, username: str, password: str) -> None:
-        self._validate_data(username, password)
-        self.username = username.strip()
-        self.password = generate_password_hash(password.strip())
+    def __init__(self, username: str, password: str, img_url: str) -> None:
+        self._validate_username(username)
+        self._validate_password(password)
+        self._validate_img_url(img_url)
 
-    def _validate_data(self, username: Any, password: Any) -> None:
-        if not self._is_username_valid(username) or not self._is_password_valid(password):
+        self.username = self._format_text(username)
+        self.password = generate_password_hash(self._format_text(password))
+        self.img_url = self._format_text(img_url)
+
+    def _validate_username(self, username: Any) -> None:
+        if not self._is_username_valid(username):
             raise CustomError('InvalidDataSent')
 
     def _is_username_valid(self, username: Any) -> bool:
+        min_length, max_length = 5, 50
+
         return (
             isinstance(username, str)
-            and len(username) >= 5
-            and re.match(r'^[a-zA-Z_\s]+$', username)
+            and len(username) >= min_length
+            and len(username) <= max_length
+            and re.match(r'^[a-zA-Z\d_-]+$', username)
         )
 
-    def _is_password_valid(self, password: Any) -> bool:
-        if not isinstance(password, str) or len(password) < 8:
-            return False
+    def _validate_password(self, password: Any) -> None:
+        if not self._is_password_valid(password):
+            raise CustomError('InvalidDataSent')
 
-        return self._password_has_necessary_chars(password)
+    def _is_password_valid(self, password: Any) -> bool:
+        min_length = 8
+
+        return (
+            isinstance(password, str)
+            and len(password) >= min_length
+            and self._password_has_necessary_chars(password)
+        )
 
     def _password_has_necessary_chars(self, password: str) -> bool:
         has_uppercase = any(char.isupper() for char in password)
@@ -44,7 +59,36 @@ class User(db.Model):
         has_digit = any(char.isdigit() for char in password)
         has_special = any(char in "!@#$%&*()_+=\-,.:;?/\\|" for char in password)
 
-        return has_uppercase and has_lowercase and has_digit and has_special
+        return all((has_uppercase, has_lowercase, has_digit, has_special))
+
+    def _validate_img_url(self, img_url: Any) -> None:
+        if not self._is_img_url_valid(img_url):
+            raise CustomError('InvalidDataSent')
+
+    def _is_img_url_valid(self, img_url: Any) -> bool:
+        min_length, max_length = 5, 255
+
+        return (
+            isinstance(img_url, str) and len(img_url) >= min_length and len(img_url) <= max_length
+        )
+
+    def _format_text(self, text: str) -> str:
+        return text.strip()
 
     def check_password(self, password: str) -> bool:
         return check_password_hash(self.password, password)
+
+    def update_username(self, username: str) -> None:
+        self._validate_username(username)
+
+        self.username = self._format_text(username)
+
+    def update_password(self, password: str) -> None:
+        self._validate_password(password)
+
+        self.password = generate_password_hash(self._format_text(password))
+
+    def update_img_url(self, img_url: str) -> None:
+        self._validate_img_url(img_url)
+
+        self.img_url = self._format_text(img_url)
