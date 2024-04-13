607e8d1e029000ce6f4336e1c8ac915f97258537
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-13 14:11:58 -0300

    feat: Criar classe BookGenresSchema no pacote schemas
    Cria classe BookGenresSchema no arquivo book_genres_schema.py no pacote schemas

---
+++ schemas/book_genres_schema.py
@@ -0,0 +1,11 @@
+from models import BookGenre
+
+from .marshmallow import ma
+
+
+class BookGenresSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = BookGenre
+
+
+book_genres_schema = BookGenresSchema()
