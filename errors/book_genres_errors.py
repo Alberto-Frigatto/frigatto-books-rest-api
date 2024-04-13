3a83bc3bb9887792bb9889bc1d6856a3c25f64f5
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-13 14:08:28 -0300

    feat: Adicionar erros para gêneros de livros em book_genres_errors.py
    Adiciona erros para gêneros de livros em book_genres_errors.py no pacote errors.

---
+++ errors/book_genres_errors.py
@@ -0,0 +1,10 @@
+errors = {
+    'BookGenreAlreadyExists': {
+        'message': 'Esse gênero de livro já existe',
+        'status': 409,
+    },
+    'BookGenreDoesntExists': {
+        'message': 'Esse gênero de livro não existe',
+        'status': 404,
+    },
+}
