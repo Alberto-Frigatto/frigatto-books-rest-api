39ec75b7b9c709f872d6ccc108a82634466078f3
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-20 11:19:58 -0300

    refactor: Refatorar classe ImageUploader
    Adiciona o atributo estático _allowed_extensions, troca a composição de strings por f-string no método _generate_random_filename, e cria o método _has_allowed_extensions.

--- image_uploaders/image_uploader.py
+++ image_uploaders/image_uploader.py
@@ -6,6 +6,7 @@ from werkzeug.datastructures import FileStorage
 
 class ImageUploader(metaclass=ABCMeta):
     _base_url = 'http://localhost:5000'
+    _allowed_extensions = '.png', '.jpg', '.jpeg'
 
     @abstractmethod
     def __init__(self, image: FileStorage) -> None:
@@ -24,7 +25,10 @@ class ImageUploader(metaclass=ABCMeta):
         pass
 
     def _generate_random_filename(self) -> str:
-        return str(uuid.uuid4()).replace('-', '') + '.jpg'
+        return f'{str(uuid.uuid4()).replace("-", "")}.jpg'
+
+    def _has_allowed_extensions(self, filename: str) -> bool:
+        return filename.lower().endswith(self._allowed_extensions)
 
     @classmethod
     @abstractmethod
