18f2fcf5ffbb48687d2f5207ac25a16d37811ee3
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-20 10:30:37 -0300

    refactor: Refatorar ResponseError para pegar automaticamente erro na api
    Altera o __init__ da classe, que agora espera um error do tipo CustomError, e a própria classe se encarrega de obter o erro da api.

--- response/response_error.py
+++ response/response_error.py
@@ -1,11 +1,16 @@
 import flask
 
+from handle_errors import CustomError
+
 from .response import Response
 
 
 class ResponseError(Response):
-    def __init__(self, api_error: dict) -> None:
-        self._api_error = api_error
+    def __init__(self, error: CustomError) -> None:
+        from api import api
+
+        self._api_error = api.errors.get(error.error_name)
+        self._error_name = error.error_name
 
     def json(self) -> flask.Response:
         status = self._api_error['status']
