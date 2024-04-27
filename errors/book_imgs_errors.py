5c390224854caeb999bf9970bc1b74c00c7ee18e
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-27 09:57:10 -0300

    feat: Adicionar erros para imagens de livros no pacote errors
    Adiciona erros para imagens de livros no arquivo book_imgs_errors.py no pacote errors.

---
+++ errors/book_imgs_errors.py
@@ -0,0 +1,10 @@
+errors = {
+    'BookImgDoesntExists': {
+        'message': 'Essa imagem de livro não existe',
+        'status': 404,
+    },
+    'BookDoesntOwnThisImg': {
+        'message': 'Essa imagem não pertence à esse livro',
+        'status': 401,
+    },
+}
