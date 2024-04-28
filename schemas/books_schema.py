825c579f885f87b317993ac67ca746b74a6fb9f6
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-27 22:02:54 -0300

    feat: Criar schema para classe Book
    Cria a classe BooksSchema no arquivo books_schema.py no pacote schemas.

---
+++ schemas/books_schema.py
@@ -0,0 +1,25 @@
+from models import Book
+
+from .book_genres_schema import BookGenresSchema
+from .book_imgs_schema import BookImgsSchema
+from .book_keywords_schema import BookKeywordsSchema
+from .book_kinds_schema import BookKindsSchema
+from .marshmallow import ma
+
+
+class BooksSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Book
+
+    id = ma.auto_field()
+    name = ma.auto_field()
+    price = ma.Float()
+    author = ma.auto_field()
+    release_year = ma.auto_field()
+    book_genre = ma.Nested(BookGenresSchema)
+    book_kind = ma.Nested(BookKindsSchema)
+    book_keywords = ma.Nested(BookKeywordsSchema, many=True)
+    book_imgs = ma.Nested(BookImgsSchema, many=True)
+
+
+books_schema = BooksSchema()
