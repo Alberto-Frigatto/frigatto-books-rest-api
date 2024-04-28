e13e0697365c84372f1bc066a4471df6ca80a4e4
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-27 22:06:46 -0300

    chore: Adicionar imports no pacote views

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,5 @@
 from .book_genres_view import book_genres_bp
+from .book_imgs_view import book_imgs_bp
 from .book_kinds_view import book_kinds_bp
+from .books_view import books_bp
 from .users_view import users_bp
