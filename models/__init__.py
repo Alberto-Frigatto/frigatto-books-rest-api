c11d779efce3ce91dcd39c90cf87d7da2b59aacb
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-27 22:04:32 -0300

    chore: Adicionar imports no pacote models

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
 from .book_genres_model import BookGenre
+from .book_imgs_model import BookImg
+from .book_keywords_model import BookKeyword
 from .book_kinds_model import BookKind
+from .books_model import Book
 from .users_model import User
