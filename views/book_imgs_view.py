377bcef6e8db862e6238679b385fd763b22c45c3
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-27 21:54:38 -0300

    feat: Criar views para imagens de livros no pacote views
    Cria no arquivo book_imgs_view.py no pacote views as classes view para o escopo das imagens dos livros.

---
+++ views/book_imgs_view.py
@@ -0,0 +1,54 @@
+from flask import Blueprint, Response, send_file
+from flask_jwt_extended import jwt_required
+from flask_restful import Api
+
+from api import BaseResource
+from controllers import BookImgsController
+from handle_errors import CustomError
+from response import ResponseError, ResponseSuccess
+from schemas import book_imgs_schema
+
+book_imgs_bp = Blueprint('books_bp', __name__)
+book_imgs_api = Api(book_imgs_bp)
+
+controller = BookImgsController()
+
+
+class GetBooksPhotosView(BaseResource):
+    def get(self, filename: str) -> Response:
+        try:
+            file_path, mimetype = controller.get_book_photo(filename)
+        except CustomError as e:
+            return ResponseError(e).json()
+        else:
+            return send_file(file_path, mimetype)
+
+
+class EditBooksPhotosView(BaseResource):
+    @jwt_required()
+    def delete(self, id_book: int, id_img: int) -> Response:
+        try:
+            controller.delete_book_img(id_book, id_img)
+        except CustomError as e:
+            return ResponseError(e).json()
+        else:
+            return ResponseSuccess().json()
+
+    @jwt_required()
+    def patch(self, id_book: int, id_img: int) -> Response:
+        try:
+            updated_book_img = controller.update_book_img(id_book, id_img)
+        except CustomError as e:
+            return ResponseError(e).json()
+        else:
+            data = book_imgs_schema.dump(updated_book_img)
+
+            return ResponseSuccess(data).json()
+
+
+book_imgs_api.add_resource(GetBooksPhotosView, '/photos/<string:filename>')
+book_imgs_api.add_resource(
+    EditBooksPhotosView,
+    '/<int:id_book>/photos/<int:id_img>',
+    '/<string:id_book>/photos/<string:id_img>',
+)
