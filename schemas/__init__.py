b39f2f24b318576aec966a0b6322c884e65c4a8c
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-04-27 22:03:12 -0300

    chore: Adicionar imports no pacote schemas

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,4 +1,6 @@
 from .book_genres_schema import book_genres_schema
+from .book_imgs_schema import book_imgs_schema
 from .book_kinds_schema import book_kinds_schema
+from .books_schema import books_schema
 from .marshmallow import ma
 from .users_schema import users_schema
