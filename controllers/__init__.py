aa6651edf1d8dea83b6569af61341b259c91d0db
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-01 13:08:47 -0300

    chore: Adicionar import à BookKeywordsController no pacote controllers

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,6 @@
 from .book_genres_controller import BookGenresController
 from .book_imgs_controller import BookImgsController
+from .book_keywords_controller import BookKeywordsController
 from .book_kinds_controller import BookKindsController
 from .books_controller import BooksController
 from .users_controller import UsersController
