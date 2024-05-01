11975fa1f48696e5d1f037283f95ec3e6f122790
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-01 13:12:24 -0300

    feat: Adicionar rota para views de palavras chave

--- routes.py
+++ routes.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
-from views import book_genres_bp, book_kinds_bp, users_bp
+from views import book_genres_bp, book_imgs_bp, book_keywords_bp, book_kinds_bp, books_bp, users_bp
 
 
 def add_routes(app: Flask):
-    app.register_blueprint(book_kinds_bp, url_prefix='/bookKinds')
-    app.register_blueprint(users_bp, url_prefix='/users')
-    app.register_blueprint(book_genres_bp, url_prefix='/bookGenres')
+    app.register_blueprint(book_kinds_bp, url_prefix='/bookKinds', name='book_kinds')
+    app.register_blueprint(users_bp, url_prefix='/users', name='users')
+    app.register_blueprint(book_genres_bp, url_prefix='/bookGenres', name='book_genres')
+    app.register_blueprint(books_bp, url_prefix='/books', name='books')
+    app.register_blueprint(book_imgs_bp, url_prefix='/books', name='book_imgs')
+    app.register_blueprint(book_keywords_bp, url_prefix='/books', name='book_keywords')
