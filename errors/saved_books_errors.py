1c1ef54c85ae878b01867806ca939786ad476593
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-06 12:18:43 -0300

    feat: Adicionar erros para livros salvos no pacote errors
    Adiciona erros para livros salvos no arquivo saved_books_errors.py no pacote errors.

---
+++ errors/saved_books_errors.py
@@ -0,0 +1,10 @@
+errors = {
+    'BookArentSaved': {
+        'message': 'O livro não foi salvo pelo usuário',
+        'status': 404,
+    },
+    'BookAlreadySaved': {
+        'message': 'O livro já está salvo pelo usuário',
+        'status': 409,
+    },
+}
