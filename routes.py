10eeead697d9abca2ea27f4d24b6ec728f262c1a
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-06 12:23:07 -0300

    feat: Adicionar rota para views de livros salvos

--- routes.py
+++ routes.py
@@ -1,6 +1,14 @@
 from flask import Flask
 
-from views import book_genres_bp, book_imgs_bp, book_keywords_bp, book_kinds_bp, books_bp, users_bp
+from views import (
+    book_genres_bp,
+    book_imgs_bp,
+    book_keywords_bp,
+    book_kinds_bp,
+    books_bp,
+    saved_books_bp,
+    users_bp,
+)
 
 
 def add_routes(app: Flask):
@@ -10,3 +18,4 @@ def add_routes(app: Flask):
     app.register_blueprint(books_bp, url_prefix='/books', name='books')
     app.register_blueprint(book_imgs_bp, url_prefix='/books', name='book_imgs')
     app.register_blueprint(book_keywords_bp, url_prefix='/books', name='book_keywords')
+    app.register_blueprint(saved_books_bp, url_prefix='/books', name='saved_books')
