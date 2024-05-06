a883bbfaeb64ca6b5bad244d56f743a38e0a0675
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-06 12:19:49 -0300

    chore: Adicionar import à classe SavedBook no pacote models

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,5 @@ from .book_imgs_model import BookImg
 from .book_keywords_model import BookKeyword
 from .book_kinds_model import BookKind
 from .books_model import Book
+from .saved_books_model import SavedBook
 from .users_model import User
