d4a6631506330d46965f65c90ea4259d63f94794
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-06 12:21:12 -0300

    chore: Adicionar import à SavedBooksController no pacote controllers

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,4 +3,5 @@ from .book_imgs_controller import BookImgsController
 from .book_keywords_controller import BookKeywordsController
 from .book_kinds_controller import BookKindsController
 from .books_controller import BooksController
+from .saved_books_controller import SavedBooksController
 from .users_controller import UsersController
