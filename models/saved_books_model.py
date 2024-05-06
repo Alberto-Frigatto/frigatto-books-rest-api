f38977b8bb1d98ab88e5fab680e9e96f69d22526
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-06 12:19:23 -0300

    feat: Criar classe SavedBook no pacote models
    Cria a classe Book no arquivo saved_books_model.py no pacote models.

---
+++ models/saved_books_model.py
@@ -0,0 +1,25 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db import db, int_pk
+from models import Book
+
+
+class SavedBook(db.Model):
+    __tablename__ = 'saved_books'
+
+    id: Mapped[int_pk]
+    id_user: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="cascade"),
+        nullable=False,
+    )
+    id_book: Mapped[int] = mapped_column(
+        ForeignKey("books.id", ondelete="cascade"),
+        nullable=False,
+    )
+
+    book: Mapped[Book] = relationship()
+
+    def __init__(self, id_user: int, book: Book) -> None:
+        self.id_user = id_user
+        self.book = book
