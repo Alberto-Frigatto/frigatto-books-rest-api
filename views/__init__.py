cdad0dcd123ba7d77c727c543f56f3d8d56e1506
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-29 13:37:29 -0300

    chore: Adicionar import à blueprint searches_bp no pacote views

--- views/__init__.py
+++ views/__init__.py
@@ -4,4 +4,5 @@ from .book_keywords_view import book_keywords_bp
 from .book_kinds_view import book_kinds_bp
 from .books_view import books_bp
 from .saved_books_view import saved_books_bp
+from .searches_view import searches_bp
 from .users_view import users_bp
