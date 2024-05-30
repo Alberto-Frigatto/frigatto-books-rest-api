dd9594ace9c027f90047a1cc2f4cf5348d69e157
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-29 22:44:58 -0300

    feat: Criar classe container BookGenreException no pacote exception
    Cria a classe BookGenreException no arquivo book_genre_exception.py.
    
    Essa classe contém as classes de exceção:
    
    - BookGenreAlreadyExists
    - BookGenreDoesntExists
    - ThereAreLinkedBooksWithThisBookGenre

---
+++ exception/book_genre_exception.py
@@ -0,0 +1,27 @@
+from .base import ApiException
+
+
+class BookGenreException:
+    class BookGenreAlreadyExists(ApiException):
+        def __init__(self, genre: str) -> None:
+            super().__init__(
+                name=self.__class__.__name__,
+                message=f'O gênero de livro "{genre}" já existe',
+                status=409,
+            )
+
+    class BookGenreDoesntExists(ApiException):
+        def __init__(self, id: int) -> None:
+            super().__init__(
+                name=self.__class__.__name__,
+                message=f'O gênero de livro {id} não existe',
+                status=404,
+            )
+
+    class ThereAreLinkedBooksWithThisBookGenre(ApiException):
+        def __init__(self, id: int) -> None:
+            super().__init__(
+                name=self.__class__.__name__,
+                message=f'O gênero de livro {id} não pode ser excluído, pois há livros desse gênero',
+                status=409,
+            )
