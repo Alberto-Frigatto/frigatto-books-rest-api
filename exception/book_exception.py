b26f09a8e72a01b4fb9be778845c2a32338feb66
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-29 22:44:02 -0300

    feat: Criar classe container BookException no pacote exception
    Cria a classe BookException no arquivo book_exception.py.
    
    Essa classe contém as classes de exceção:
    
    - BookAlreadyExists
    - BookDoesntExists

---
+++ exception/book_exception.py
@@ -0,0 +1,19 @@
+from .base import ApiException
+
+
+class BookException:
+    class BookAlreadyExists(ApiException):
+        def __init__(self, name: str) -> None:
+            super().__init__(
+                name=self.__class__.__name__,
+                message=f'O livro "{name}" já existe',
+                status=409,
+            )
+
+    class BookDoesntExists(ApiException):
+        def __init__(self, id: int) -> None:
+            super().__init__(
+                name=self.__class__.__name__,
+                message=f'O livro {id} não existe',
+                status=404,
+            )
