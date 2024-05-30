1ef0e9fe5a1367806a2e37fc5b4ed72afec83495
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-05-29 22:51:33 -0300

    feat: Criar classe container SavedBookException no pacote exception
    Cria a classe SavedBookException no arquivo saved_book_exception.py.
    
    Essa classe contém as classes de exceção:
    
    - BookArentSaved
    - BookAlreadySaved

---
+++ exception/saved_book_exception.py
@@ -0,0 +1,19 @@
+from .base import ApiException
+
+
+class SavedBookException:
+    class BookArentSaved(ApiException):
+        def __init__(self, id: int) -> None:
+            super().__init__(
+                name=self.__class__.__name__,
+                message=f'O livro {id} não foi salvo pelo usuário',
+                status=404,
+            )
+
+    class BookAlreadySaved(ApiException):
+        def __init__(self, id: int) -> None:
+            super().__init__(
+                name=self.__class__.__name__,
+                message=f'O livro {id} já está salvo pelo usuário',
+                status=409,
+            )
