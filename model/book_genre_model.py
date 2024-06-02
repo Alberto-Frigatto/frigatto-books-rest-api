ba1f65dac2678fef1f1166e2e3d73423682b2ee3
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-02 13:12:57 -0300

    refactor: Excluir validações e formatações de texto em BookGenre

--- model/book_genre_model.py
+++ model/book_genre_model.py
@@ -5,7 +5,6 @@ from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from db import db, int_pk
-from exception import GeneralException
 
 
 class BookGenre(db.Model):
@@ -15,30 +14,7 @@ class BookGenre(db.Model):
     genre: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
 
     def __init__(self, genre: str) -> None:
-        self._validate_genre_name(genre)
-        self.genre = self._format_genre(genre)
-
-    def _validate_genre_name(self, genre: Any) -> None:
-        if not self._is_genre_name_valid(genre):
-            raise GeneralException.InvalidDataSent()
-
-    def _is_genre_name_valid(self, genre: Any) -> bool:
-        if not isinstance(genre, str):
-            return False
-
-        min_length, max_length = 3, 30
-        genre_length = len(genre)
-
-        return (
-            genre_length >= min_length
-            and genre_length <= max_length
-            and re.match(r'^[a-zA-ZáàãâäéèẽêëíìîĩïóòõôöúùũûüÁÀÃÂÄÉÈẼÊËÍÌÎĨÏÓÒÕÔÖÚÙŨÛÜç\s]+$', genre)
-        )
-
-    def _format_genre(self, genre: str) -> str:
-        return genre.strip().lower()
+        self.genre = genre
 
     def update_genre(self, genre: str) -> None:
-        self._validate_genre_name(genre)
-
-        self.genre = self._format_genre(genre)
+        self.genre = genre
