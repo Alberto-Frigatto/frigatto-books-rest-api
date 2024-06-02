549107cebd34c66e8f4e7b93ce67daac507ba763
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-02 12:59:12 -0300

    feat: Criar classe UpdateBookGenreDTO no pacote dto.input
    Cria a classe UpdateBookGenreDTO no arquivo create_book_genre_dto.py no pacote dto.input.

---
+++ dto/input/update_book_genre_dto.py
@@ -0,0 +1,28 @@
+from dto.base import InputDTO
+from dto.constraints import StrConstraints
+from exception import GeneralException
+
+
+class UpdateBookGenreDTO(InputDTO):
+    genre: str
+
+    def __init__(self) -> None:
+        data = super()._get_json_data()
+        self._validate_data(data)
+        self._set_fields(data)
+
+    def _validate_data(self, data: dict) -> None:
+        new_genre = data.get('genre')
+
+        if not (
+            isinstance(new_genre, str)
+            and StrConstraints.not_empty(new_genre)
+            and StrConstraints.between_size(new_genre, min_size=3, max_size=30)
+            and StrConstraints.match_pattern(
+                new_genre, r'^[a-zA-ZáàãâäéèẽêëíìîĩïóòõôöúùũûüÁÀÃÂÄÉÈẼÊËÍÌÎĨÏÓÒÕÔÖÚÙŨÛÜç\s]+$'
+            )
+        ):
+            raise GeneralException.InvalidDataSent()
+
+    def _set_fields(self, data: dict) -> None:
+        self.genre = data['genre'].strip().lower()
