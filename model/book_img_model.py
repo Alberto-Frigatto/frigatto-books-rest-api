692d03e3ddbc3b58eae0b549080a3904f2f728bd
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-02 13:13:28 -0300

    refactor: Excluir validações e formatações de texto em BookImg

--- model/book_img_model.py
+++ model/book_img_model.py
@@ -4,7 +4,6 @@ from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from db import db, int_pk
-from exception import GeneralException
 
 
 class BookImg(db.Model):
@@ -19,27 +18,7 @@ class BookImg(db.Model):
     )
 
     def __init__(self, img_url: str) -> None:
-        self._validate_img_url(img_url)
-        self.img_url = self._format_img_url(img_url)
-
-    def _validate_img_url(self, img_url: Any) -> None:
-        if not self._is_img_url_valid(img_url):
-            raise GeneralException.InvalidDataSent()
-
-    def _is_img_url_valid(self, img_url: Any) -> bool:
-        if not isinstance(img_url, str):
-            return False
-
-        min_length, max_length = 5, 255
-
-        img_url_length = len(img_url)
-
-        return img_url_length >= min_length and img_url_length <= max_length
-
-    def _format_img_url(self, img_url: str) -> str:
-        return img_url.strip().lower()
+        self.img_url = img_url
 
     def update_img_url(self, img_url: str) -> None:
-        self._validate_img_url(img_url)
-
-        self.img_url = self._format_img_url(img_url)
+        self.img_url = img_url
