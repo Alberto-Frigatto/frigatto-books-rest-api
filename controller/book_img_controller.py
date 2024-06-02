7a4c26f27ab1fced472183885d16b379fd4da93b
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-02 13:29:27 -0300

    refactor: Alterar classe BookImgController para receber os DTO's
    Altera os métodos:
    
    - create_book_img: Recebe como parâmetro um objeto CreateBookImgDTO; verificações dos dados da requisição são removidas;
    
    - update_book_img: Recebe como parâmetro um objeto UpdateBookImgDTO; verificações dos dados da requisição são removidas;
    
    E cria o método _swap_book_img.

--- controller/book_img_controller.py
+++ controller/book_img_controller.py
@@ -3,6 +3,7 @@ import os
 from flask import current_app, request
 
 from db import db
+from dto.input import CreateBookImgDTO, UpdateBookImgDTO
 from exception import BookException, BookImgException, GeneralException, ImageException
 from image_uploader import BookImageUploader
 from model import Book, BookImg
@@ -62,53 +63,33 @@ class BookImgController(Controller):
 
         return book_img
 
-    def update_book_img(self, id_book: str, id_img: str) -> BookImg:
-        if not super().are_there_data():
-            raise GeneralException.NoDataSent()
-
-        files_data = request.files.to_dict()
-
-        if not self._is_data_valid(files_data):
-            raise GeneralException.InvalidDataSent()
-
+    def update_book_img(self, *, id_book: str, id_img: str, input_dto: UpdateBookImgDTO) -> BookImg:
         book = self._get_book_by_id(id_book)
         book_img = self._get_book_img_by_id(id_img)
 
         if book_img.id_book != book.id:
             raise BookImgException.BookDoesntOwnThisImg(id_img, id_book)
 
-        image_uploader = BookImageUploader(files_data['img'])
-
-        BookImageUploader.delete(book_img.img_url)
-
-        book_img.update_img_url(image_uploader.get_url())
-
-        image_uploader.save()
+        self._swap_book_img(book_img, input_dto.img)
 
         db.session.commit()
 
         return book_img
 
-    def _is_data_valid(self, files_data: dict) -> bool:
-        return 'img' in files_data.keys()
+    def _swap_book_img(self, old_img: BookImg, new_img: BookImageUploader) -> None:
+        BookImageUploader.delete(old_img.img_url)
 
-    def create_book_img(self, id_book: str) -> BookImg:
-        if not super().are_there_data():
-            raise GeneralException.NoDataSent()
+        old_img.update_img_url(new_img.get_url())
 
-        files_data = request.files.to_dict()
+        new_img.save()
 
-        if not self._is_data_valid(files_data):
-            raise GeneralException.InvalidDataSent()
-
-        image_uploader = BookImageUploader(files_data['img'])
-
+    def create_book_img(self, id_book: str, input_dto: CreateBookImgDTO) -> BookImg:
         book = self._get_book_by_id(id_book)
-        book_img = BookImg(image_uploader.get_url())
+        book_img = BookImg(input_dto.img.get_url())
 
         book.book_imgs.append(book_img)
 
-        image_uploader.save()
+        input_dto.img.save()
 
         db.session.commit()
 
