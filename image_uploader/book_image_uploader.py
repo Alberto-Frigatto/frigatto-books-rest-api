037ed76b23726879ecdb80b5f9eb67d3ddc72275
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-02 13:09:15 -0300

    refactor: Alterar classe BookImageUploader
    Renomeia o método _validate_file para validate_file e o transforma em um método estático, cria o método _has_valid_extensions como método estático, e remove a validação de arquivo no construtor da classe.

--- image_uploader/book_image_uploader.py
+++ image_uploader/book_image_uploader.py
@@ -1,40 +1,36 @@
 import os
-from typing import Any
 
 from flask import current_app
 from werkzeug.datastructures import FileStorage
-
-from exception import GeneralException
 
 from .image_uploader import ImageUploader
 
 
 class BookImageUploader(ImageUploader):
     def __init__(self, image: FileStorage) -> None:
-        self._validate_file(image)
-
         self._file = image
         self._new_filename = self._generate_random_filename()
 
-    def _validate_file(self, file: Any) -> None:
-        if not self._is_file_valid(file):
-            raise GeneralException.InvalidDataSent()
+    def get_url(self) -> str:
+        return f'{super()._base_url}/books/photos/{self._new_filename}'
 
-    def _is_file_valid(self, file: FileStorage) -> bool:
+    def save(self) -> None:
+        filename = os.path.join(current_app.config['BOOK_PHOTOS_UPLOAD_DIR'], self._new_filename)
+        self._file.save(filename)
+
+    @classmethod
+    def validate_file(cls, file: FileStorage) -> None:
         max_size = current_app.config['BOOK_PHOTOS_MAX_SIZE']
 
         file.stream.seek(0, 2)
         file_size = file.stream.tell()
         file.stream.seek(0)
 
-        return self._has_allowed_extensions(file.filename) and file_size <= max_size
+        return cls._has_valid_extensions(file.filename) and file_size <= max_size
 
-    def get_url(self) -> str:
-        return f'{self._base_url}/books/photos/{self._new_filename}'
-
-    def save(self) -> None:
-        filename = os.path.join(current_app.config['BOOK_PHOTOS_UPLOAD_DIR'], self._new_filename)
-        self._file.save(filename)
+    @classmethod
+    def _has_valid_extensions(cls, filename: str) -> bool:
+        return filename.lower().endswith(cls._allowed_extensions)
 
     @classmethod
     def delete(cls, img_url: str) -> None:
