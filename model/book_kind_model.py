00ec1ca98e671886ffc748593852db4eee2f3750
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-02 13:13:08 -0300

    refactor: Excluir validações e formatações de texto em BookKind

--- model/book_kind_model.py
+++ model/book_kind_model.py
@@ -5,7 +5,6 @@ from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from db import db, int_pk
-from exception import GeneralException
 
 
 class BookKind(db.Model):
@@ -15,30 +14,7 @@ class BookKind(db.Model):
     kind: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
 
     def __init__(self, kind: str) -> None:
-        self._validate_kind_name(kind)
-        self.kind = self._format_kind_name(kind)
-
-    def _validate_kind_name(self, kind: Any) -> None:
-        if not self._is_kind_name_valid(kind):
-            raise GeneralException.InvalidDataSent()
-
-    def _is_kind_name_valid(self, kind: Any) -> bool:
-        if not isinstance(kind, str):
-            return False
-
-        min_length, max_length = 3, 30
-        kind_length = len(kind)
-
-        return (
-            kind_length >= min_length
-            and kind_length <= max_length
-            and re.match(r'^[a-zA-ZáàãâäéèẽêëíìîĩïóòõôöúùũûüÁÀÃÂÄÉÈẼÊËÍÌÎĨÏÓÒÕÔÖÚÙŨÛÜç\s]+$', kind)
-        )
-
-    def _format_kind_name(self, kind: str) -> str:
-        return kind.strip().lower()
+        self.kind = kind
 
     def update_kind(self, kind: str) -> None:
-        self._validate_kind_name(kind)
-
-        self.kind = self._format_kind_name(kind)
+        self.kind = kind
