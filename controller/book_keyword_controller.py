b254006904e63b78073e1860ee206a15a05d637a
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-08 19:07:57 -0300

    refactor: Alterar classe BookKeywordController para receber os DTO's
    Altera os métodos:
    
    - create_book_keyword: Recebe como parâmetro um objeto CreateBookKeywordDTO; verificações dos dados da requisição são removidas;

--- controller/book_keyword_controller.py
+++ controller/book_keyword_controller.py
@@ -1,33 +1,23 @@
 from typing import Any
 
 from db import db
-from exception import BookException, BookKeywordException, GeneralException
+from dto.input import CreateBookKeywordDTO
+from exception import BookException, BookKeywordException
 from model import Book, BookKeyword
 
 from .controller import Controller
 
 
 class BookKeywordController(Controller):
-    def create_book_keyword(self, id_book: str) -> BookKeyword:
-        if not super().are_there_data():
-            raise GeneralException.NoDataSent()
-
-        data = super().get_json_data()
-
-        if not self._is_data_valid(data):
-            raise GeneralException.InvalidDataSent()
-
+    def create_book_keyword(self, id_book: str, input_dto: CreateBookKeywordDTO) -> BookKeyword:
         book = self._get_book_by_id(id_book)
-        book_keyword = BookKeyword(data['keyword'])
+        book_keyword = BookKeyword(input_dto.keyword)
 
         book.book_keywords.append(book_keyword)
 
         db.session.commit()
 
         return book_keyword
-
-    def _is_data_valid(self, data: Any) -> bool:
-        return isinstance(data, dict) and 'keyword' in data.keys()
 
     def _get_book_by_id(self, id: str) -> Book:
         book = db.session.get(Book, id)
