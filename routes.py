42dcd7038b3d37cdc2d0d311fba275b1347d6c7d
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-11 15:23:44 -0300

    refactor: Renomear blueprints importadas em routes.py

--- routes.py
+++ routes.py
@@ -2,22 +2,24 @@ from flask import Flask
 
 from view import (
     book_genres_bp,
-    book_imgs_bp,
-    book_keywords_bp,
-    book_kinds_bp,
-    books_bp,
-    saved_books_bp,
-    searches_bp,
-    users_bp,
+    book_bp,
+    book_genre_bp,
+    book_img_bp,
+    book_keyword_bp,
+    book_kind_bp,
+    saved_book_bp,
+    search_bp,
+    user_bp,
 )
 
 
 def add_routes(app: Flask):
     app.register_blueprint(book_kinds_bp, url_prefix='/bookKinds', name='book_kinds')
-    app.register_blueprint(users_bp, url_prefix='/users', name='users')
-    app.register_blueprint(book_genres_bp, url_prefix='/bookGenres', name='book_genres')
-    app.register_blueprint(books_bp, url_prefix='/books', name='books')
-    app.register_blueprint(book_imgs_bp, url_prefix='/books', name='book_imgs')
-    app.register_blueprint(book_keywords_bp, url_prefix='/books', name='book_keywords')
-    app.register_blueprint(saved_books_bp, url_prefix='/books', name='saved_books')
-    app.register_blueprint(searches_bp, url_prefix='/search', name='searches')
+    app.register_blueprint(book_bp, url_prefix='/books', name='books')
+    app.register_blueprint(book_genre_bp, url_prefix='/bookGenres', name='book_genres')
+    app.register_blueprint(book_img_bp, url_prefix='/books', name='book_imgs')
+    app.register_blueprint(book_keyword_bp, url_prefix='/books', name='book_keywords')
+    app.register_blueprint(book_kind_bp, url_prefix='/bookKinds', name='book_kinds')
+    app.register_blueprint(saved_book_bp, url_prefix='/books', name='saved_books')
+    app.register_blueprint(search_bp, url_prefix='/search', name='searches')
+    app.register_blueprint(user_bp, url_prefix='/users', name='users')
