f3db4314678322cf23c1091f679a138702e98a5a
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-11 15:14:31 -0300

    refactor: Alterar view para pesquisa de livros
    Altera as seguintes coisas:
    
    - Renomeia o arquivo para search_view.py
    - Cria uma única classe SearchView com todos os endpoints
    - Exclui o uso do flask-restful
    - renomeia a blueprint para search_bp

--- view/search_view.py
+++ view/search_view.py
@@ -1,21 +1,20 @@
 from flask import Blueprint, Response
-from flask_restful import Api
 
-from api import BaseResource
 from controller import SearchController
 from dto.input import SearchDTO
 from exception.base import ApiException
 from response import ResponseError, ResponseSuccess
 from schema import books_schema
 
-searches_bp = Blueprint('searches_bp', __name__)
-searches_api = Api(searches_bp)
+search_bp = Blueprint('search_bp', __name__)
 
 controller = SearchController()
 
 
-class SearchersView(BaseResource):
-    def get(self) -> Response:
+class SearchView:
+    @staticmethod
+    @search_bp.get('')
+    def search_books() -> Response:
         try:
             input_dto = SearchDTO()
             matched_books = controller.search_books(input_dto)
@@ -25,6 +24,3 @@ class SearchersView(BaseResource):
             data = books_schema.dump(matched_books, many=True)
 
             return ResponseSuccess(data).json()
-
-
-searches_api.add_resource(SearchersView, '')
