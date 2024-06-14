12ab09f6dfade011c0b742a4e8aaa6b374b9b699
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-14 20:06:38 -0300

    refactor: Adicionar uso de classes repository à BookKeywordController
    Adiciona o uso das classes BookKeywordRepository e BookRepository na classe BookKeywordController.

--- controller/book_keyword_controller.py
+++ controller/book_keyword_controller.py
@@ -1,32 +1,26 @@
-from db import db
 from dto.input import CreateBookKeywordDTO
-from exception import BookException, BookKeywordException
+from exception import BookKeywordException
 from model import Book, BookKeyword
+from repository import BookKeywordRepository, BookRepository
 
 
 class BookKeywordController:
+    book_keyword_repository = BookKeywordRepository()
+    book_repository = BookRepository()
+
     def create_book_keyword(self, id_book: str, input_dto: CreateBookKeywordDTO) -> BookKeyword:
-        book = self._get_book_by_id(id_book)
+        book = self.book_repository.get_by_id(id_book)
         book_keyword = BookKeyword(input_dto.keyword)
+        book_keyword.id_book = book.id
 
-        book.book_keywords.append(book_keyword)
-
-        db.session.commit()
+        self.book_keyword_repository.add(book_keyword)
 
         return book_keyword
 
-    def _get_book_by_id(self, id: str) -> Book:
-        book = db.session.get(Book, id)
-
-        if book is None:
-            raise BookException.BookDoesntExists(id)
-
-        return book
-
     def delete_book_keyword(self, id_book: str, id_keyword: str) -> None:
-        book = self._get_book_by_id(id_book)
+        book = self.book_repository.get_by_id(id_book)
 
-        book_keyword = self._get_book_keyword_by_id(id_keyword)
+        book_keyword = self.book_keyword_repository.get_by_id(id_keyword)
 
         if book_keyword.id_book != book.id:
             raise BookKeywordException.BookDoesntOwnThisKeyword(id_keyword, id_book)
@@ -34,16 +28,7 @@ class BookKeywordController:
         if self._does_book_have_one_keyword(book):
             raise BookKeywordException.BookMustHaveAtLeastOneKeyword(id_book)
 
-        db.session.delete(book_keyword)
-        db.session.commit()
-
-    def _get_book_keyword_by_id(self, id: str) -> BookKeyword:
-        book = db.session.get(BookKeyword, id)
-
-        if book is None:
-            raise BookKeywordException.BookKeywordDoesntExists(id)
-
-        return book
+        self.book_keyword_repository.delete(book_keyword)
 
     def _does_book_have_one_keyword(self, book: Book) -> bool:
         return len(book.book_keywords) == 1
