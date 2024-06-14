c518f501d7d3acf10aa5ce17d674c1c2b5c75d17
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-14 20:04:22 -0300

    refactor: Adicionar uso de BookKindRepository à BookKindController

--- controller/book_kind_controller.py
+++ controller/book_kind_controller.py
@@ -1,65 +1,33 @@
 from typing import Sequence
 
-from sqlalchemy import select
-
-from db import db
 from dto.input import CreateBookKindDTO, UpdateBookKindDTO
-from exception import BookKindException
-from model import Book, BookKind
+from model import BookKind
+from repository import BookKindRepository
 
 
 class BookKindController:
-    def get_all_book_kinds(self) -> Sequence[BookKind]:
-        query = select(BookKind).order_by(BookKind.id)
-        book_kinds = db.session.execute(query).scalars().all()
+    repository = BookKindRepository()
 
-        return book_kinds
+    def get_all_book_kinds(self) -> Sequence[BookKind]:
+        return self.repository.get_all()
 
     def get_book_kind_by_id(self, id: str) -> BookKind:
-        book_kind = db.session.get(BookKind, id)
-
-        if book_kind is None:
-            raise BookKindException.BookKindDoesntExists(id)
-
-        return book_kind
+        return self.repository.get_by_id(id)
 
     def create_book_kind(self, input_dto: CreateBookKindDTO) -> BookKind:
         new_book_kind = BookKind(input_dto.kind)
 
-        if self._book_kind_already_exists(input_dto.kind):
-            raise BookKindException.BookKindAlreadyExists(input_dto.kind)
-
-        db.session.add(new_book_kind)
-        db.session.commit()
+        self.repository.add(new_book_kind)
 
         return new_book_kind
 
-    def _book_kind_already_exists(self, book_kind: str) -> bool:
-        query = select(BookKind).filter_by(kind=book_kind)
-
-        return bool(db.session.execute(query).scalar())
-
     def delete_book_kind(self, id: str) -> None:
-        book_kind = self.get_book_kind_by_id(id)
-
-        if self._are_there_linked_books(book_kind):
-            raise BookKindException.ThereAreLinkedBooksWithThisBookKind(id)
-
-        db.session.delete(book_kind)
-        db.session.commit()
-
-    def _are_there_linked_books(self, book_kind: BookKind) -> bool:
-        query = select(Book).filter_by(id_kind=book_kind.id)
-        return bool(db.session.execute(query).scalars().all())
+        self.repository.delete(id)
 
     def update_book_kind(self, id: str, input_dto: UpdateBookKindDTO) -> BookKind:
-        book_kind = self.get_book_kind_by_id(id)
-        new_kind = input_dto.kind
+        book_kind = self.repository.get_by_id(id)
+        book_kind.update_kind(input_dto.kind)
 
-        if self._book_kind_already_exists(new_kind):
-            raise BookKindException.BookKindAlreadyExists(new_kind)
-
-        book_kind.update_kind(new_kind)
-        db.session.commit()
+        self.repository.update(book_kind)
 
         return book_kind
