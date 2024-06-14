8864f25050733d16a1f3bf2c17948129a4026f46
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-14 20:07:06 -0300

    refactor: Adicionar uso de UserRepository à AuthController

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -1,22 +1,21 @@
-from typing import Any
-
 from flask_jwt_extended import create_access_token, current_user
-from sqlalchemy import select
 
-from db import db
 from dto.input import LoginDTO
 from exception import AuthException
 from model import User
+from repository import UserRepository
 
 token = str
 
 
 class AuthController:
+    repository = UserRepository()
+
     def login(self, input_dto: LoginDTO) -> tuple[User, token]:
         if self._user_already_authenticated():
             raise AuthException.UserAlreadyAuthenticated()
 
-        user = self._get_user_by_username(input_dto.username)
+        user = self.repository.get_by_username(input_dto.username)
 
         if user is None or not user.check_password(input_dto.password):
             raise AuthException.InvalidLogin()
@@ -27,8 +26,3 @@ class AuthController:
 
     def _user_already_authenticated(self) -> bool:
         return bool(current_user)
-
-    def _get_user_by_username(self, username: Any) -> User | None:
-        query = select(User).filter_by(username=username)
-
-        return db.session.execute(query).scalar()
