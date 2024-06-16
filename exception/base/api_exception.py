0d641fd72380ebe379477f117ac1347aeb89e429
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-16 13:12:50 -0300

    refactor: Alterar type hint do parâmetro message da classe ApiException

--- exception/base/api_exception.py
+++ exception/base/api_exception.py
@@ -1,8 +1,9 @@
 from abc import ABCMeta
+from typing import Any
 
 
 class ApiException(Exception, metaclass=ABCMeta):
-    def __init__(self, *, name: str, message: str, status: int) -> None:
+    def __init__(self, *, name: str, message: str | Any, status: int) -> None:
         self.name = name
         self.message = message
         self.status = status
