5a83aa0af20b5042a03e56f078cd762794685bcc
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-16 15:56:26 -0300

    refactor: Alterar classe CreateBookGenreDTO para BookGenreInputDTO
    Altera as seguintes coisas:
    
    - Renomeia o arquivo de create_book_genre_dto.py para book_genre_input_dto.py
    - Renomeia a classe de CreateBookGenreDTO para BookGenreInputDTO
    - Transforma a classe num model do pydantic

---
+++ dto/input/book_genre_input_dto.py
@@ -0,0 +1,22 @@
+from typing import Annotated, Any
+
+from pydantic import StringConstraints
+
+from dto.base import InputDTO
+
+
+class BookGenreInputDTO(InputDTO):
+    genre: Annotated[
+        str,
+        StringConstraints(
+            strict=True,
+            strip_whitespace=True,
+            to_lower=True,
+            min_length=3,
+            max_length=30,
+            pattern=r'^[a-zA-ZÀ-ÿç\s]+$',
+        ),
+    ]
+
+    def __init__(self, **data: str | Any) -> None:
+        super().__init__(**data)
