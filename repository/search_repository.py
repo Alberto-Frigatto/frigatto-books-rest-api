a9e9769164f1698a8ae6a946fda7527e364801de
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-16 16:15:04 -0300

    refactor: Alterar métodos da classe SearchRepository

--- repository/search_repository.py
+++ repository/search_repository.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from typing import Sequence
 
 from sqlalchemy import Select, select
@@ -13,19 +14,50 @@ class SearchRepository:
     book_kind_repository = BookKindRepository()
     book_genre_repository = BookGenreRepository()
 
-    def search(self, query: str | None, filter: dict) -> Sequence[Book]:
-        sql_query = self._build_query(query, filter)
+    def search(
+        self,
+        *,
+        query: str | None,
+        id_book_kind: int | None,
+        id_book_genre: int | None,
+        release_year: int | None,
+        min_price: Decimal | None,
+        max_price: Decimal | None,
+    ) -> Sequence[Book]:
+        sql_query = self._build_query(
+            query, id_book_kind, id_book_genre, release_year, min_price, max_price
+        )
 
         return db.session.execute(sql_query).scalars().all()
 
-    def _build_query(self, search_query: str | None, search_filter: dict | None) -> select_book:
+    def _build_query(
+        self,
+        search_query: str | None,
+        id_book_kind: int | None,
+        id_book_genre: int | None,
+        release_year: int | None,
+        min_price: Decimal | None,
+        max_price: Decimal | None,
+    ) -> select_book:
         sql_query = select(Book).order_by(Book.id)
 
         if search_query is not None:
             sql_query = self._apply_search_query(sql_query, search_query)
 
-        if search_filter is not None:
-            sql_query = self._apply_filters(sql_query, search_filter)
+        if id_book_kind is not None:
+            sql_query = self._apply_kind(sql_query, id_book_kind)
+
+        if id_book_genre is not None:
+            sql_query = self._apply_genre(sql_query, id_book_genre)
+
+        if release_year is not None:
+            sql_query = self._apply_release_year(sql_query, release_year)
+
+        if min_price is not None:
+            sql_query = self._apply_min_price(sql_query, min_price)
+
+        if max_price is not None:
+            sql_query = self._apply_max_price(sql_query, max_price)
 
         return sql_query
 
@@ -41,66 +73,35 @@ class SearchRepository:
             | Book.book_keywords.any(keyword=search_query.lower())
         )
 
-    def _apply_filters(
-        self,
-        sql_query: select_book,
-        search_filter: dict,
-    ) -> select_book:
-        filters = (
-            self._apply_kind_filter,
-            self._apply_genre_filter,
-            self._apply_release_year_filter,
-            self._apply_price_filter,
-        )
-
-        for filter in filters:
-            sql_query = filter(sql_query, search_filter)
-
-        return sql_query
-
-    def _apply_kind_filter(self, sql_query: select_book, search_filter: dict) -> select_book:
-        id_kind: int | None = search_filter.get('id_kind', None)
-
-        if id_kind and self._book_kind_exists(id_kind):
+    def _apply_kind(self, sql_query: select_book, id_kind: int) -> select_book:
+        if self._book_kind_exists(id_kind):
             sql_query = sql_query.filter_by(id_kind=id_kind)
-
-        return sql_query
-
-    def _apply_genre_filter(self, sql_query: select_book, search_filter: dict) -> select_book:
-        id_genre: int | None = search_filter.get('id_genre', None)
-
-        if id_genre and self._book_genre_exists(id_genre):
-            sql_query = sql_query.filter_by(id_genre=id_genre)
-
-        return sql_query
-
-    def _apply_release_year_filter(
-        self, sql_query: select_book, search_filter: dict
-    ) -> select_book:
-        release_year: int | None = search_filter.get('release_year', None)
-
-        if release_year:
-            sql_query = sql_query.filter_by(release_year=release_year)
-
-        return sql_query
-
-    def _apply_price_filter(self, sql_query: select_book, search_filter: dict) -> select_book:
-        price: dict | None = search_filter.get('price', None)
-
-        if price is not None:
-            min_price: int | float | None = price.get('min', None)
-            max_price: int | float | None = price.get('max', None)
-
-            if min_price is not None:
-                sql_query = sql_query.where(Book.price >= min_price)
-
-            if max_price is not None:
-                sql_query = sql_query.where(Book.price <= max_price)
 
         return sql_query
 
     def _book_kind_exists(self, id: int | None) -> bool:
         return bool(self.book_kind_repository.get_by_id(str(id) if id is not None else ''))
 
+    def _apply_genre(self, sql_query: select_book, id_genre: int) -> select_book:
+        if self._book_genre_exists(id_genre):
+            sql_query = sql_query.filter_by(id_genre=id_genre)
+
+        return sql_query
+
     def _book_genre_exists(self, id: int | None) -> bool:
         return bool(self.book_genre_repository.get_by_id(str(id) if id is not None else ''))
+
+    def _apply_release_year(self, sql_query: select_book, release_year: int) -> select_book:
+        sql_query = sql_query.filter_by(release_year=release_year)
+
+        return sql_query
+
+    def _apply_min_price(self, sql_query: select_book, min_price: Decimal) -> select_book:
+        sql_query = sql_query.where(Book.price >= min_price)
+
+        return sql_query
+
+    def _apply_max_price(self, sql_query: select_book, max_price: Decimal) -> select_book:
+        sql_query = sql_query.where(Book.price <= max_price)
+
+        return sql_query
