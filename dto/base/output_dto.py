1abe8d3beb060d683dbcd98ac2296bec9e25c3d9
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-23 23:23:14 -0300

    feat: Criar classe OutputDTO no pacote dto.base
    Cria a classe OutputDTO no arquivo output_dto.py no pacote dto.base.
    
    Essa classe será usada como super classe de todas as classes DTO de saída.

---
+++ dto/base/output_dto.py
@@ -0,0 +1,24 @@
+from typing import Any, Sequence
+
+from pydantic import BaseModel, ConfigDict
+
+from db import db
+
+Model = db.Model
+
+
+class OutputDTO(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    @classmethod
+    def dump(cls, model: Model) -> dict[str, Any]:
+        dto = cls(**model.__dict__)
+        serialization = dto.model_dump()
+
+        return serialization
+
+    @classmethod
+    def dump_many(cls, models: Sequence[Model]) -> list[dict[str, Any]]:
+        serialization = [cls.dump(model) for model in models]
+
+        return serialization
