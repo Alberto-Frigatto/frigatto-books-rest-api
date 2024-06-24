e2d70cccc64e5d0f693f387f748475c4e93a80d5
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-24 00:04:27 -0300

    refactor: Excluir pacote schema

--- schema/__init__.py
+++
@@ -1,7 +0,0 @@
-from .book_genres_schema import book_genres_schema
-from .book_imgs_schema import book_imgs_schema
-from .book_keywords_schema import book_keywords_schema
-from .book_kinds_schema import book_kinds_schema
-from .books_schema import books_schema
-from .marshmallow import ma
-from .users_schema import users_schema
