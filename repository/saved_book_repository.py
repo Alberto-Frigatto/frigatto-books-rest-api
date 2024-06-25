c7eacf8c29ee56f458442a1156499eff97ac8eed
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-25 13:10:07 -0300

    refactor: Injetar sessão do database em SavedBookRepository

--- repository/saved_book_repository.py
+++ repository/saved_book_repository.py
@@ -1,17 +1,22 @@
 from typing import Sequence
 
 from flask_jwt_extended import current_user
+from injector import inject
 from sqlalchemy import select
+from sqlalchemy.orm import scoped_session
 
-from db import db
 from exception import SavedBookException
 from model import Book, SavedBook
 
 
+@inject
 class SavedBookRepository:
+    def __init__(self, session: scoped_session) -> None:
+        self.session = session
+
     def get_all(self) -> list[Book]:
         query = select(SavedBook).filter_by(id_user=current_user.id).order_by(SavedBook.id)
-        saved_books: Sequence[SavedBook] = db.session.execute(query).scalars().all()
+        saved_books: Sequence[SavedBook] = self.session.execute(query).scalars().all()
 
         return [saved_book.book for saved_book in saved_books]
 
@@ -19,19 +24,19 @@ class SavedBookRepository:
         if self._saved_book_already_exists(new_saved_book.book):
             raise SavedBookException.BookAlreadySaved(str(new_saved_book.book.id))
 
-        db.session.add(new_saved_book)
-        db.session.commit()
+        self.session.add(new_saved_book)
+        self.session.commit()
 
     def _saved_book_already_exists(self, saved_book: Book) -> bool:
         return saved_book in self.get_all()
 
     def delete(self, saved_book: SavedBook) -> None:
-        db.session.delete(saved_book)
-        db.session.commit()
+        self.session.delete(saved_book)
+        self.session.commit()
 
     def get_by_id_book(self, id_book: int) -> SavedBook:
         query = select(SavedBook).filter_by(id_book=id_book)
-        saved_book = db.session.execute(query).scalar()
+        saved_book = self.session.execute(query).scalar()
 
         if saved_book is None:
             raise SavedBookException.BookArentSaved(str(id_book))
