9edb64dec132ff287e87121a4b504ccadedd31f4
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-25 13:28:30 -0300

    refactor: Alterar classe SearchView
    Injeta SearchController nos edpoints e remove os blocos try-except.

--- view/search_view.py
+++ view/search_view.py
@@ -3,25 +3,19 @@ from flask import Blueprint, Response
 from controller import SearchController
 from dto.input import SearchInputDTO
 from dto.output import BookOutputDTO
-from exception.base import ApiException
 from request import Request
-from response import ResponseError, ResponseSuccess
+from response import ResponseSuccess
 
 search_bp = Blueprint('search_bp', __name__)
-
-controller = SearchController()
 
 
 class SearchView:
     @staticmethod
     @search_bp.get('')
-    def search_books() -> Response:
-        try:
-            input_dto = SearchInputDTO(**Request.get_json())
-            matched_books = controller.search_books(input_dto)
-        except ApiException as e:
-            return ResponseError(e).json()
-        else:
-            data = BookOutputDTO.dump_many(matched_books)
+    def search_books(controller: SearchController) -> Response:
+        input_dto = SearchInputDTO(**Request.get_json())
 
-            return ResponseSuccess(data).json()
+        matched_books = controller.search_books(input_dto)
+        data = BookOutputDTO.dump_many(matched_books)
+
+        return ResponseSuccess(data).json()
