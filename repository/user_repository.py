20225e1a85975b6b2cfd752b1176c939f535bc8c
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-25 13:10:18 -0300

    refactor: Injetar sessão do database em UserRepository

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,23 +1,28 @@
+from injector import inject
 from sqlalchemy import select
+from sqlalchemy.orm import scoped_session
 
-from db import db
 from exception import UserException
 from model import User
 
 
+@inject
 class UserRepository:
+    def __init__(self, session: scoped_session) -> None:
+        self.session = session
+
     def add(self, new_user: User) -> None:
         if self._user_already_exists(new_user.username):
             raise UserException.UserAlreadyExists()
 
-        db.session.add(new_user)
-        db.session.commit()
+        self.session.add(new_user)
+        self.session.commit()
 
     def _user_already_exists(self, username: str | None) -> bool:
-        with db.session.no_autoflush:
+        with self.session.no_autoflush:
             query = select(User).where(User.username.ilike(username))
 
-            return bool(db.session.execute(query).scalar())
+            return bool(self.session.execute(query).scalar())
 
     def update(self, updated_user: User) -> None:
         if self._was_username_modified(updated_user) and self._user_already_exists(
@@ -25,16 +30,16 @@ class UserRepository:
         ):
             raise UserException.UserAlreadyExists()
 
-        db.session.commit()
+        self.session.commit()
 
     def _was_username_modified(self, updated_user: User) -> bool:
-        with db.session.no_autoflush:
+        with self.session.no_autoflush:
             query = select(User.username).filter_by(id=updated_user.id)
-            old_username = db.session.execute(query).scalar()
+            old_username = self.session.execute(query).scalar()
 
             return updated_user.username != old_username
 
     def get_by_username(self, username: str) -> User | None:
         query = select(User).filter_by(username=username)
 
-        return db.session.execute(query).scalar()
+        return self.session.execute(query).scalar()
