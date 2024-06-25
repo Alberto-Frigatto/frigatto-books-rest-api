e2f8328626cd6288b79e03cab4d9fd17c245d544
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-25 12:33:49 -0300

    feat: Criar app_dev.py no pacote config com configs de desenvolvimento do app

---
+++ config/app_dev.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'key'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+JWT_SECRET_KEY = 'key'
+JWT_TOKEN_LOCATION = ['headers']
+JWT_HEADER_TYPE = 'Bearer'
+TESTING = True
+USER_PHOTOS_UPLOAD_DIR = 'tests/uploads'
+BOOK_PHOTOS_UPLOAD_DIR = 'tests/uploads'
