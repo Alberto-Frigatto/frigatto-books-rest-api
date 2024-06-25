3e64b580f45a92ca20b920236ff8b85cfae07398
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-25 13:27:57 -0300

    refactor: Alterar classe SavedBookView
    Injeta SavedBookController nos edpoints e remove os blocos try-except.

--- view/saved_book_view.py
+++ view/saved_book_view.py
@@ -3,33 +3,26 @@ from flask_jwt_extended import jwt_required
 
 from controller import SavedBookController
 from dto.output import BookOutputDTO
-from exception.base import ApiException
-from response import ResponseError, ResponseSuccess
+from response import ResponseSuccess
 
 saved_book_bp = Blueprint('saved_book_bp', __name__)
 
 
 class SavedBookView:
-    controller = SavedBookController()
-
     @staticmethod
     @saved_book_bp.post('/<id>/save')
     @jwt_required()
-    def save_book(id: str) -> Response:
-        try:
-            saved_book = SavedBookView.controller.save_book(id)
-        except ApiException as e:
-            return ResponseError(e).json()
-        else:
-            data = BookOutputDTO.dump(saved_book)
+    def save_book(id: str, controller: SavedBookController) -> Response:
+        saved_book = controller.save_book(id)
+        data = BookOutputDTO.dump(saved_book)
 
-            return ResponseSuccess(data, 201).json()
+        return ResponseSuccess(data, 201).json()
 
     @staticmethod
     @saved_book_bp.get('/saved')
     @jwt_required()
-    def get_all_saved_books() -> Response:
-        saved_books = SavedBookView.controller.get_all_saved_books()
+    def get_all_saved_books(controller: SavedBookController) -> Response:
+        saved_books = controller.get_all_saved_books()
         data = BookOutputDTO.dump_many(saved_books)
 
         return ResponseSuccess(data).json()
@@ -37,10 +30,7 @@ class SavedBookView:
     @staticmethod
     @saved_book_bp.delete('/saved/<id_book>')
     @jwt_required()
-    def delete_saved_book(id_book: str) -> Response:
-        try:
-            SavedBookView.controller.delete_saved_book(id_book)
-        except ApiException as e:
-            return ResponseError(e).json()
-        else:
-            return ResponseSuccess().json()
+    def delete_saved_book(id_book: str, controller: SavedBookController) -> Response:
+        controller.delete_saved_book(id_book)
+
+        return ResponseSuccess().json()
