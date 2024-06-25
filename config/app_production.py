57896a42fa5fb617df982cbb9da92136b73bee22
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-25 12:34:08 -0300

    feat: Criar app_production.py no pacote config com configs de produção do app

---
+++ config/app_production.py
@@ -0,0 +1,10 @@
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{os.getenv("DB_USER", "root")}:{os.getenv("DB_PWD")}@localhost:3306/frigatto_books'
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+JWT_TOKEN_LOCATION = ['cookies']
+UPLOAD_DIR = 'uploads'
+USER_PHOTOS_UPLOAD_DIR = f'{UPLOAD_DIR}/users_photos'
+BOOK_PHOTOS_UPLOAD_DIR = f'{UPLOAD_DIR}/books_photos'
+PROPAGATE_EXCEPTIONS = True
