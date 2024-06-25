7c282d46ad3bef5097dda62d9a580719ebb8e054
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-25 13:09:09 -0300

    refactor: Injetar sessão do database em BookImgRepository

--- repository/book_img_repository.py
+++ repository/book_img_repository.py
@@ -1,12 +1,18 @@
-from db import db
+from injector import inject
+from sqlalchemy.orm import scoped_session
+
 from exception import BookImgException
 from image_uploader import BookImageUploader
 from model import BookImg
 
 
+@inject
 class BookImgRepository:
+    def __init__(self, session: scoped_session) -> None:
+        self.session = session
+
     def get_by_id(self, id: str | int) -> BookImg:
-        book_img = db.session.get(BookImg, id)
+        book_img = self.session.get(BookImg, id)
 
         if book_img is None:
             raise BookImgException.BookImgDoesntExists(str(id))
@@ -14,14 +20,14 @@ class BookImgRepository:
         return book_img
 
     def add(self, new_book_img: BookImg) -> None:
-        db.session.add(new_book_img)
-        db.session.commit()
+        self.session.add(new_book_img)
+        self.session.commit()
 
     def delete(self, book_img: BookImg) -> None:
-        db.session.delete(book_img)
-        db.session.commit()
+        self.session.delete(book_img)
+        self.session.commit()
 
         BookImageUploader.delete(book_img.img_url)
 
     def update(self) -> None:
-        db.session.commit()
+        self.session.commit()
