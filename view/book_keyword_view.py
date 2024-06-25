778f69bc8d6d7e743d95954535ddd21be5159c3b
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-25 13:25:01 -0300

    refactor: Alterar classe BookKeywordView
    Injeta BookKeywordController nos edpoints e remove os blocos try-except.

--- view/book_keyword_view.py
+++ view/book_keyword_view.py
@@ -4,37 +4,30 @@ from flask_jwt_extended import jwt_required
 from controller import BookKeywordController
 from dto.input import BookKeywordInputDTO
 from dto.output import BookKeywordOutputDTO
-from exception.base import ApiException
 from request import Request
-from response import ResponseError, ResponseSuccess
+from response import ResponseSuccess
 
 book_keyword_bp = Blueprint('book_keyword_bp', __name__)
 
 
 class BookKeywordView:
-    controller = BookKeywordController()
-
     @staticmethod
     @book_keyword_bp.post('/<id_book>/keywords')
     @jwt_required()
-    def create_book_keyword(id_book: str) -> Response:
-        try:
-            input_dto = BookKeywordInputDTO(**Request.get_json())
-            new_book_keyword = BookKeywordView.controller.create_book_keyword(id_book, input_dto)
-        except ApiException as e:
-            return ResponseError(e).json()
-        else:
-            data = BookKeywordOutputDTO.dump(new_book_keyword)
+    def create_book_keyword(id_book: str, controller: BookKeywordController) -> Response:
+        input_dto = BookKeywordInputDTO(**Request.get_json())
 
-            return ResponseSuccess(data, 201).json()
+        new_book_keyword = controller.create_book_keyword(id_book, input_dto)
+        data = BookKeywordOutputDTO.dump(new_book_keyword)
+
+        return ResponseSuccess(data, 201).json()
 
     @staticmethod
     @book_keyword_bp.delete('/<id_book>/keywords/<id_keyword>')
     @jwt_required()
-    def delete_book_keyword(id_book: str, id_keyword: str) -> Response:
-        try:
-            BookKeywordView.controller.delete_book_keyword(id_book, id_keyword)
-        except ApiException as e:
-            return ResponseError(e).json()
-        else:
-            return ResponseSuccess().json()
+    def delete_book_keyword(
+        id_book: str, id_keyword: str, controller: BookKeywordController
+    ) -> Response:
+        controller.delete_book_keyword(id_book, id_keyword)
+
+        return ResponseSuccess().json()
