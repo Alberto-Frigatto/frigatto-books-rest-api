819ba31f13111b49f97e5ace2589364c5b18b4b7
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-26 13:53:40 -0300

    feat: Criar interface ISavedBookService no pacote service
    Cria a interface ISavedBookService no arquivo i_saved_book_service.py no pacote service.

---
+++ service/i_saved_book_service.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+from model import Book
+
+
+class ISavedBookService(ABC):
+    @abstractmethod
+    def get_all_saved_books(self) -> Sequence[Book]:
+        pass
+
+    @abstractmethod
+    def save_book(self, id: str) -> Book:
+        pass
+
+    @abstractmethod
+    def delete_saved_book(self, id_book: str) -> None:
+        pass
