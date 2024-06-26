d4242d8c45902ac3e145b5927dd284e711474b48
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-26 14:37:25 -0300

    feat: Criar interface IBookGenreController no pacote controller
    Cria a interface IBookGenreController no arquivo i_book_genre_controller.py no pacote controller.

---
+++ controller/i_book_genre_controller.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+from dto.input import BookGenreInputDTO
+from model import BookGenre
+
+
+class IBookGenreController(ABC):
+    @abstractmethod
+    def get_all_book_genres(self) -> Sequence[BookGenre]:
+        pass
+
+    @abstractmethod
+    def get_book_genre_by_id(self, id: str) -> BookGenre:
+        pass
+
+    @abstractmethod
+    def create_book_genre(self, input_dto: BookGenreInputDTO) -> BookGenre:
+        pass
+
+    @abstractmethod
+    def delete_book_genre(self, id: str) -> None:
+        pass
+
+    @abstractmethod
+    def update_book_genre(self, id: str, input_dto: BookGenreInputDTO) -> BookGenre:
+        pass
