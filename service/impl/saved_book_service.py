d4555a1af69c1dd084463c6719fc987711a739d7
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-26 14:03:45 -0300

    feat: Criar classe SavedBookService no pacote service.impl
    Cria a classe concreta SavedBookService no arquivo saved_book_service.py no pacote service.impl que implementa a interface ISavedBookService.

---
+++ service/impl/saved_book_service.py
@@ -0,0 +1,35 @@
+from typing import Sequence
+
+from flask_jwt_extended import current_user
+from injector import inject
+
+from model import Book, SavedBook
+from repository import IBookRepository, ISavedBookRepository
+
+from .. import ISavedBookService
+
+
+@inject
+class SavedBookService(ISavedBookService):
+    def __init__(
+        self, book_repository: IBookRepository, saved_book_repository: ISavedBookRepository
+    ) -> None:
+        self.book_repository = book_repository
+        self.saved_book_repository = saved_book_repository
+
+    def get_all_saved_books(self) -> Sequence[Book]:
+        return self.saved_book_repository.get_all()
+
+    def save_book(self, id: str) -> Book:
+        book = self.book_repository.get_by_id(id)
+        saved_book = SavedBook(current_user.id, book)
+
+        self.saved_book_repository.add(saved_book)
+
+        return book
+
+    def delete_saved_book(self, id_book: str) -> None:
+        book = self.book_repository.get_by_id(id_book)
+        saved_book = self.saved_book_repository.get_by_id_book(str(book.id))
+
+        self.saved_book_repository.delete(saved_book)
