bb90d2c2ea82ce006c7cd99a6b45438e49e052cb
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-26 14:02:11 -0300

    feat: Criar classe BookKindService no pacote service.impl
    Cria a classe concreta BookKindService no arquivo book_kind_service.py no pacote service.impl que implementa a interface IBookKindService.

---
+++ service/impl/book_kind_service.py
@@ -0,0 +1,39 @@
+from typing import Sequence
+
+from injector import inject
+
+from dto.input import BookKindInputDTO
+from model import BookKind
+from repository import IBookKindRepository
+
+from .. import IBookKindService
+
+
+@inject
+class BookKindService(IBookKindService):
+    def __init__(self, repository: IBookKindRepository) -> None:
+        self.repository = repository
+
+    def get_all_book_kinds(self) -> Sequence[BookKind]:
+        return self.repository.get_all()
+
+    def get_book_kind_by_id(self, id: str) -> BookKind:
+        return self.repository.get_by_id(id)
+
+    def create_book_kind(self, input_dto: BookKindInputDTO) -> BookKind:
+        new_book_kind = BookKind(input_dto.kind)
+
+        self.repository.add(new_book_kind)
+
+        return new_book_kind
+
+    def delete_book_kind(self, id: str) -> None:
+        self.repository.delete(id)
+
+    def update_book_kind(self, id: str, input_dto: BookKindInputDTO) -> BookKind:
+        book_kind = self.repository.get_by_id(id)
+        book_kind.update_kind(input_dto.kind)
+
+        self.repository.update(book_kind)
+
+        return book_kind
