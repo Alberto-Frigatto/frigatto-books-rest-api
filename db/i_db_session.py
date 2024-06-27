61052a1709b29804a5ebba28e5dcbaef74ae49c8
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-26 21:54:01 -0300

    feat: Criar interface IDbSession no pacote db
    Cria a interface IDbSession no arquivo i_db_session.py no pacote db.

---
+++ db/i_db_session.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from typing import Any, Sequence, TypeVar
+
+from sqlalchemy import Select
+
+from .database import db
+
+TModel = TypeVar("TModel", db.Model, Any)
+Model = db.Model
+
+
+class IDbSession(ABC):
+    @abstractmethod
+    def get_by_id(self, model: type[TModel], id: str) -> type[TModel] | None:
+        pass
+
+    @abstractmethod
+    def get_one(self, query: Select[tuple[type[TModel]]]) -> type[TModel] | None:
+        pass
+
+    @abstractmethod
+    def get_many(self, query: Select[tuple[type[TModel]]]) -> Sequence[type[TModel]]:
+        pass
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def add(self, model: Model) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, model: Model) -> None:
+        pass
