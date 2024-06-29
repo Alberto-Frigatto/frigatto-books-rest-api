92b592d40a33d23106bf234fb5cd373357bcaff3
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-29 13:00:09 -0300

    refactor: Traduzir mensagens de erro para inglês em SavedBookException

--- exception/saved_book_exception.py
+++ exception/saved_book_exception.py
@@ -6,7 +6,7 @@ class SavedBookException:
         def __init__(self, id: str) -> None:
             super().__init__(
                 name=self.__class__.__name__,
-                message=f'O livro {id} não foi salvo pelo usuário',
+                message=f'The book {id} was not saved by the user',
                 status=404,
             )
 
@@ -14,6 +14,6 @@ class SavedBookException:
         def __init__(self, id: str) -> None:
             super().__init__(
                 name=self.__class__.__name__,
-                message=f'O livro {id} já está salvo pelo usuário',
+                message=f'The book {id} is already saved by the user',
                 status=409,
             )
