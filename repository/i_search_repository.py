e955af693a7022bce3e7fb569d0b9918c3646bda
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-29 14:41:00 -0300

    refactor: Alterar método search da interface ISearchRepository
    Altera o método search da interface ISearchRepository para receber um parâmetro page e devolver um objeto Pagination.

--- repository/i_search_repository.py
+++ repository/i_search_repository.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
 from decimal import Decimal
-from typing import Sequence
 
-from model import Book
+from flask_sqlalchemy.pagination import Pagination
 
 
 class ISearchRepository(ABC):
@@ -10,11 +9,12 @@ class ISearchRepository(ABC):
     def search(
         self,
         *,
+        page: int,
         query: str | None,
         id_book_kind: int | None,
         id_book_genre: int | None,
         release_year: int | None,
         min_price: Decimal | None,
         max_price: Decimal | None,
-    ) -> Sequence[Book]:
+    ) -> Pagination:
         pass
