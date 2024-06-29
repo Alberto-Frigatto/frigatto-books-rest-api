e15a4aebc86d75e75dc4b6221b0347e08c4446e4
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-29 12:59:16 -0300

    refactor: Traduzir mensagens de erro para inglês em BookImgException

--- exception/book_img_exception.py
+++ exception/book_img_exception.py
@@ -6,7 +6,7 @@ class BookImgException:
         def __init__(self, id: str) -> None:
             super().__init__(
                 name=self.__class__.__name__,
-                message=f'A imagem {id} não existe',
+                message=f'The image {id} does not exist',
                 status=404,
             )
 
@@ -14,7 +14,7 @@ class BookImgException:
         def __init__(self, id_img: str, id_book: str) -> None:
             super().__init__(
                 name=self.__class__.__name__,
-                message=f'A imagem {id_img} não pertence ao livro {id_book}',
+                message=f'The image {id_img} does not belong to the book {id_book}',
                 status=401,
             )
 
@@ -22,6 +22,6 @@ class BookImgException:
         def __init__(self, id_book: str) -> None:
             super().__init__(
                 name=self.__class__.__name__,
-                message=f'O livro {id_book} deve ter pelo menos uma imagem',
+                message=f'The book {id_book} must have at least one image',
                 status=400,
             )
