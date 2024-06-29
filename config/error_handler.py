6e8a954e4efb64d008d543cdf8d8cd58703c2983
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-29 14:34:29 -0300

    refactor: Adicionar uso de ErrorResponse em error_handler.py

--- config/error_handler.py
+++ config/error_handler.py
@@ -1,10 +1,10 @@
 from flask import Flask, Response
 
 from exception.base import ApiException
-from response import ResponseError
+from response import ErrorResponse
 
 
 def add_error_handlers(app: Flask) -> None:
     @app.errorhandler(ApiException)
     def handle_exceptions(e) -> Response:
-        return ResponseError(e).json()
+        return ErrorResponse(e).json()
