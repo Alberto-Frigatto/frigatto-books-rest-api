57289c8e5db92234225b34b8fa689a8fe219b06e
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-29 14:48:33 -0300

    refactor: Alterar método get_all_book_genres da classe BookGenreService

--- service/impl/book_genre_service.py
+++ service/impl/book_genre_service.py
@@ -1,5 +1,4 @@
-from typing import Sequence
-
+from flask_sqlalchemy.pagination import Pagination
 from injector import inject
 
 from dto.input import BookGenreInputDTO
@@ -14,8 +13,8 @@ class BookGenreService(IBookGenreService):
     def __init__(self, repository: IBookGenreRepository) -> None:
         self.repository = repository
 
-    def get_all_book_genres(self) -> Sequence[BookGenre]:
-        return self.repository.get_all()
+    def get_all_book_genres(self, page: int) -> Pagination:
+        return self.repository.get_all(page)
 
     def get_book_genre_by_id(self, id: str) -> BookGenre:
         return self.repository.get_by_id(id)
