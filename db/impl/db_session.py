b8ecfe2283636bf82c8b4a9d250540aede299948
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-29 14:37:28 -0300

    refactor: Alterar classe DbSession
    Altera o método __init__ para receber um objeto SQLAlchemy , altera os métodos necessários para usar esse objeto e adiciona o método paginate.

--- db/impl/db_session.py
+++ db/impl/db_session.py
@@ -1,8 +1,11 @@
 from typing import Any, Sequence, TypeVar
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.pagination import Pagination
 from injector import inject
 from sqlalchemy import Select
-from sqlalchemy.orm import scoped_session
+
+from exception import GeneralException
 
 from .. import IDbSession, db
 
@@ -12,28 +15,39 @@ Model = db.Model
 
 @inject
 class DbSession(IDbSession):
-    def __init__(self, session: scoped_session) -> None:
-        self.session = session
+    def __init__(self, db: SQLAlchemy) -> None:
+        self.db = db
+
+    def paginate(self, query: Select[tuple[type[TModel]]], *, page: int) -> Pagination:
+        with self.db.session.no_autoflush:
+            try:
+                return self.db.paginate(
+                    query,
+                    page=page,
+                    per_page=20,
+                )
+            except Exception as e:
+                raise GeneralException.PaginationPageDoesNotExists(page)
 
     def get_by_id(self, model: type[TModel], id: str) -> type[TModel] | None:
-        with self.session.no_autoflush:
-            return self.session.get(model, id)
+        with self.db.session.no_autoflush:
+            return self.db.session.get(model, id)
 
     def get_one(self, query: Select[tuple[type[TModel]]]) -> type[TModel] | None:
-        with self.session.no_autoflush:
-            return self.session.execute(query).scalar()
+        with self.db.session.no_autoflush:
+            return self.db.session.execute(query).scalar()
 
     def get_many(self, query: Select[tuple[type[TModel]]]) -> Sequence[type[TModel]]:
-        with self.session.no_autoflush:
-            return self.session.execute(query).scalars().all()
+        with self.db.session.no_autoflush:
+            return self.db.session.execute(query).scalars().all()
 
     def update(self) -> None:
-        self.session.commit()
+        self.db.session.commit()
 
     def add(self, model: Model) -> None:
-        self.session.add(model)
-        self.session.commit()
+        self.db.session.add(model)
+        self.db.session.commit()
 
     def delete(self, model: Model) -> None:
-        self.session.delete(model)
-        self.session.commit()
+        self.db.session.delete(model)
+        self.db.session.commit()
