623f5a34d03c43bef39d0d3d8344712b86cef4e7
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-06-29 14:39:25 -0300

    refactor: Alterar método get_all da interface IBookGenreRepository
    Altera o método get_all da interface IBookGenreRepository para receber um parâmetro page e devolver um objeto Pagination.

--- repository/i_book_genre_repository.py
+++ repository/i_book_genre_repository.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Sequence
+
+from flask_sqlalchemy.pagination import Pagination
 
 from model import BookGenre
 
 
 class IBookGenreRepository(ABC):
     @abstractmethod
-    def get_all(self) -> Sequence[BookGenre]:
+    def get_all(self, page: int) -> Pagination:
         pass
 
     @abstractmethod
