15237f1b9e7cfe24bbc18000cd5d78ddff3f7f7b
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 15:25:24 -0300

    refactor: Alterar método cast_img_to_UserImageUploader de BookImgInputDTO

--- dto/input/book_img_input_dto.py
+++ dto/input/book_img_input_dto.py
@@ -1,8 +1,13 @@
+from typing import Any
+
+from flask import current_app
 from pydantic import ConfigDict, field_validator
 from werkzeug.datastructures import FileStorage
 
 from dto.base import InputDTO
-from image_uploader import BookImageUploader
+from exception import ImageException
+from utils.file.uploader import BookImageUploader
+from utils.file.validator import BookImageValidator
 
 
 class BookImgInputDTO(InputDTO):
@@ -11,11 +16,20 @@ class BookImgInputDTO(InputDTO):
     model_config = ConfigDict(arbitrary_types_allowed=True, extra='forbid')
 
     @field_validator('img', mode='plain')
-    def cast_img_to_UserImageUploader(cls, img: FileStorage) -> BookImageUploader:
-        if not BookImageUploader.validate_file(img):
-            raise ValueError('img is larger than 7MB or is not a image')
+    @classmethod
+    def cast_img_to_UserImageUploader(cls, img: Any) -> BookImageUploader:
+        if not BookImageValidator.is_a_file(img):
+            raise ImageException.ImageIsNotAFile()
+
+        if not BookImageValidator.has_valid_extension(img):
+            raise ImageException.FileIsNotAnImage
+
+        if not BookImageValidator.has_valid_size(img):
+            max_file_size: int = current_app.config["BOOK_PHOTOS_MAX_SIZE"] // int(1e6)
+
+            raise ImageException.ImageIsTooLarge(max_file_size)
 
         return BookImageUploader(img)
 
-    def __init__(self, **data: FileStorage) -> None:
+    def __init__(self, **data: FileStorage | Any) -> None:
         super().__init__(**data)
