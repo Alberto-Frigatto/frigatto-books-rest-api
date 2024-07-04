3c97578550cc3f475f602d6e5545fbe4512115a0
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 14:37:22 -0300

    refactor: Alterar classe UpdateUserInputDTO
    Altera as seguintes coisas na classe:
    
    - Renomeia o método password_has_necessary_chars para password_has_necessary_length_and_chars, adicionando verificação de tamanho à senha
    
    - Alterar o método cast_img_to_UserImageUploader para utilizar UserImageValidator e retornar exceções de ImageException

--- dto/input/update_user_input_dto.py
+++ dto/input/update_user_input_dto.py
@@ -1,11 +1,13 @@
 from typing import Annotated, Any, Optional
 
-from pydantic import BaseModel, ConfigDict, StringConstraints, ValidationError, field_validator
+from flask import current_app
+from pydantic import ConfigDict, StringConstraints, field_validator
 from werkzeug.datastructures import FileStorage
 
 from dto.base import InputDTO
-from exception import GeneralException
-from image_uploader import UserImageUploader
+from exception import ImageException, UserException
+from utils.file.uploader import UserImageUploader
+from utils.file.validator import UserImageValidator
 
 
 class UpdateUserInputDTO(InputDTO):
@@ -39,24 +41,39 @@ class UpdateUserInputDTO(InputDTO):
     model_config = ConfigDict(arbitrary_types_allowed=True, extra='forbid')
 
     @field_validator('password', mode='plain')
-    def password_has_necessary_chars(cls, password: str) -> str:
+    @classmethod
+    def password_has_necessary_length_and_chars(cls, password: str) -> str:
+        min_chars = 8
+        if len(password) < min_chars:
+            raise UserException.PasswordTooShort(min_chars)
+
+        max_chars = 255
+        if len(password) > max_chars:
+            raise UserException.PasswordTooLong(max_chars)
+
         has_uppercase = any(char.isupper() for char in password)
         has_lowercase = any(char.islower() for char in password)
         has_digit = any(char.isdigit() for char in password)
         has_special = any(char in "!@#$%&*()_+=-,.:;?/\\|" for char in password)
 
         if not all((has_uppercase, has_lowercase, has_digit, has_special)):
-            raise ValueError('password has not the necessary chars')
+            raise UserException.PasswordWithoutNecessaryChars()
 
         return password
 
     @field_validator('img', mode='plain')
+    @classmethod
     def cast_img_to_UserImageUploader(cls, img: Any) -> UserImageUploader:
-        if not isinstance(img, FileStorage):
-            raise ValueError('img is not a file')
+        if not UserImageValidator.is_a_file(img):
+            raise ImageException.ImageIsNotAFile()
 
-        if not UserImageUploader.validate_file(img):
-            raise ValueError('img is larger than 5MB or is not an image')
+        if not UserImageValidator.has_valid_extension(img):
+            raise ImageException.FileIsNotAnImage
+
+        if not UserImageValidator.has_valid_size(img):
+            max_file_size: int = current_app.config["USER_PHOTOS_MAX_SIZE"] // int(1e6)
+
+            raise ImageException.ImageIsTooLarge(max_file_size)
 
         return UserImageUploader(img)
 
