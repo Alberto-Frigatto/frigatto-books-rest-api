dea6bf821ab73a4d452c6dabebac72953766e1b7
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 14:33:16 -0300

    refactor: Alterar PositiveInt para Field em atributos de SearchInputDTO

--- dto/input/search_input_dto.py
+++ dto/input/search_input_dto.py
@@ -1,16 +1,16 @@
 from decimal import Decimal
 from typing import Annotated, Any, Optional
 
-from pydantic import Field, PositiveInt
+from pydantic import Field
 
 from dto.base import InputDTO
 
 
 class SearchInputDTO(InputDTO):
     query: Optional[str] = None
-    id_book_kind: Optional[Annotated[int, PositiveInt]] = None
-    id_book_genre: Optional[Annotated[int, PositiveInt]] = None
-    release_year: Optional[Annotated[int, PositiveInt]] = None
+    id_book_kind: Optional[Annotated[int, Field(gt=0)]] = None
+    id_book_genre: Optional[Annotated[int, Field(gt=0)]] = None
+    release_year: Optional[Annotated[int, Field(gt=0)]] = None
     min_price: Optional[Annotated[Decimal, Field(gt=0, max_digits=6, decimal_places=2)]] = None
     max_price: Optional[Annotated[Decimal, Field(gt=0, max_digits=6, decimal_places=2)]] = None
 
