068eaae75c49fbc876ca04e7dea40f186f415d52
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 13:04:08 -0300

    refactor: Alterar construtor das exceções de BookImgException

--- exception/book_img_exception.py
+++ exception/book_img_exception.py
@@ -5,7 +5,6 @@ class BookImgException:
     class BookImgDoesntExists(ApiException):
         def __init__(self, id: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The image {id} does not exist',
                 status=404,
             )
@@ -13,7 +12,6 @@ class BookImgException:
     class BookDoesntOwnThisImg(ApiException):
         def __init__(self, id_img: str, id_book: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The image {id_img} does not belong to the book {id_book}',
                 status=401,
             )
@@ -21,7 +19,6 @@ class BookImgException:
     class BookMustHaveAtLeastOneImg(ApiException):
         def __init__(self, id_book: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The book {id_book} must have at least one image',
                 status=400,
             )
