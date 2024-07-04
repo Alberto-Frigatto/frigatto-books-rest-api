268d6cec514df2147caba53fa8f0909f0512ccc5
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 15:58:19 -0300

    refactor: Adicionar callbacks de erros de jwt em config/error_handler.py

--- config/error_handler.py
+++ config/error_handler.py
@@ -1,6 +1,8 @@
 from flask import Flask, Response
 
+from exception import SecurityException
 from exception.base import ApiException
+from security import jwt
 from utils.response import ErrorResponse
 
 
@@ -8,3 +10,20 @@ def add_error_handlers(app: Flask) -> None:
     @app.errorhandler(ApiException)
     def handle_exceptions(e) -> Response:
         return ErrorResponse(e).json()
+
+    @jwt.unauthorized_loader
+    def unauthorized_callback(error_string: str) -> Response:
+        error_map = {
+            'CSRF double submit tokens do not match': 'InvalidCSFR',
+            'Missing CSRF token': 'MissingCSFR',
+            'Missing cookie "access_token_cookie"': 'MissingJWT',
+            'Missing Authorization Header': 'MissingJWT',
+        }
+
+        exception: ApiException = getattr(SecurityException, error_map[error_string])()
+
+        return ErrorResponse(exception).json()
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error_string: str) -> Response:
+        return ErrorResponse(SecurityException.InvalidJWT()).json()
