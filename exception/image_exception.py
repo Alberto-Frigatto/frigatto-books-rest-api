724a67a8d54d02c91e11e6f62c97891c634bddf0
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 13:09:08 -0300

    refactor: Alterar e adicionar classes à ImageException
    Altera o construtor da classe ImageNotFound removendo o parâmetro name.
    
    Adiciona as classes ImagesArentFiles, FileIsNotAnImage, ImageIsNotAFile e ImageIsTooLarge que herdam de ValidationException.

--- exception/image_exception.py
+++ exception/image_exception.py
@@ -1,11 +1,26 @@
-from .base import ApiException
+from .base import ApiException, ValidationException
 
 
 class ImageException:
     class ImageNotFound(ApiException):
         def __init__(self, filename: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The image {filename} was not found',
                 status=404,
             )
+
+    class ImagesArentFiles(ValidationException):
+        def __init__(self) -> None:
+            super().__init__('The provided images are not files')
+
+    class ImageIsNotAFile(ValidationException):
+        def __init__(self) -> None:
+            super().__init__('The provided image is not a file')
+
+    class FileIsNotAnImage(ValidationException):
+        def __init__(self) -> None:
+            super().__init__('The provided file is not an image')
+
+    class ImageIsTooLarge(ValidationException):
+        def __init__(self, max_size_mb: int) -> None:
+            super().__init__(f'The provided image is larger than {max_size_mb}MB')
