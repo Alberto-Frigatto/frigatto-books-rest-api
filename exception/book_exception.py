e53745c5eb7d0b0cc4045a0eb0220bfb2588dea5
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 13:07:07 -0300

    refactor: Alterar e adicionar classes à BookException
    Altera o construtor das classes BookAlreadyExists e BookDoesntExists removendo o parâmetro name.
    
    Adiciona as classes BookAlreadyHaveImageMaxQty, BookImageListTooShort e BookImageListTooLong que herdam de ValidationException.

--- exception/book_exception.py
+++ exception/book_exception.py
@@ -1,19 +1,32 @@
-from .base import ApiException
+from .base import ApiException, ValidationException
 
 
 class BookException:
     class BookAlreadyExists(ApiException):
         def __init__(self, name: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
-                message=f'The book "{name}" already exists',
+                message=f'The book "{name.strip().title()}" already exists',
                 status=409,
+            )
+
+    class BookAlreadyHaveImageMaxQty(ApiException):
+        def __init__(self, name: str) -> None:
+            super().__init__(
+                message=f'The book "{name}" already have the max quantity of images',
+                status=400,
             )
 
     class BookDoesntExists(ApiException):
         def __init__(self, id: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The book {id} does not exist',
                 status=404,
             )
+
+    class BookImageListTooShort(ValidationException):
+        def __init__(self, min_qty: int) -> None:
+            super().__init__(f'Book must contains at least {min_qty} image')
+
+    class BookImageListTooLong(ValidationException):
+        def __init__(self, max_qty: int) -> None:
+            super().__init__(f'Book must contains at most {max_qty} images')
