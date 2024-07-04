c0cf84edcac2fd43617abebdcd27bdc547beb1de
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 13:10:14 -0300

    refactor: Alterar e adicionar classes à UserException
    Altera o construtor da classe UserAlreadyExists removendo o parâmetro name.
    
    Adiciona as classes PasswordTooShort, PasswordTooLong e PasswordWithoutNecessaryChars que herdam de ValidationException.

--- exception/user_exception.py
+++ exception/user_exception.py
@@ -1,11 +1,22 @@
-from .base import ApiException
+from .base import ApiException, ValidationException
 
 
 class UserException:
     class UserAlreadyExists(ApiException):
         def __init__(self) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message='This user already exists',
                 status=409,
             )
+
+    class PasswordTooShort(ValidationException):
+        def __init__(self, min_chars: int) -> None:
+            super().__init__(f'The provided password should have at least {min_chars} characters')
+
+    class PasswordTooLong(ValidationException):
+        def __init__(self, max_chars: int) -> None:
+            super().__init__(f'The provided password should have at most {max_chars} characters')
+
+    class PasswordWithoutNecessaryChars(ValidationException):
+        def __init__(self) -> None:
+            super().__init__('The provided password has not the necessary chars')
