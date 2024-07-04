911001f1ad014e26c6a02ebf5b9cea9d7a15caae
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 13:04:42 -0300

    refactor: Alterar construtor das exceções de SavedBookException

--- exception/saved_book_exception.py
+++ exception/saved_book_exception.py
@@ -5,7 +5,6 @@ class SavedBookException:
     class BookArentSaved(ApiException):
         def __init__(self, id: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The book {id} was not saved by the user',
                 status=404,
             )
@@ -13,7 +12,6 @@ class SavedBookException:
     class BookAlreadySaved(ApiException):
         def __init__(self, id: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The book {id} is already saved by the user',
                 status=409,
             )
