17ea8afe24a1d0f5592596a6f00f60ca89528e03
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 13:41:26 -0300

    refactor: Alterar import à classe ErrorResponse em error_handler.py

--- config/error_handler.py
+++ config/error_handler.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response
 
 from exception.base import ApiException
-from response import ErrorResponse
+from utils.response import ErrorResponse
 
 
 def add_error_handlers(app: Flask) -> None:
