adae5b90e62d29c723a18a054edd582f2c1daf64
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 13:04:17 -0300

    refactor: Alterar construtor das exceções de BookKindException

--- exception/book_kind_exception.py
+++ exception/book_kind_exception.py
@@ -5,7 +5,6 @@ class BookKindException:
     class BookKindAlreadyExists(ApiException):
         def __init__(self, kind: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The book kind "{kind}" already exists',
                 status=409,
             )
@@ -13,7 +12,6 @@ class BookKindException:
     class BookKindDoesntExists(ApiException):
         def __init__(self, id: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The book kind {id} does not exist',
                 status=404,
             )
@@ -21,7 +19,6 @@ class BookKindException:
     class ThereAreLinkedBooksWithThisBookKind(ApiException):
         def __init__(self, id: str) -> None:
             super().__init__(
-                name=self.__class__.__name__,
                 message=f'The book genre {id} cannot be deleted because there are books linked to this kind',
                 status=409,
             )
