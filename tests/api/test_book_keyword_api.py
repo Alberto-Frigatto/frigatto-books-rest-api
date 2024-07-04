7f1b3ed9edffa7a1437568a9be42f4b48d038ffd
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-04 12:22:03 -0300

    test: Mover, renomear e refatorar testes para palavras chave de livros
    Move o arquivo test_book_keyword.py para a pasta tests/api, o renomeia para test_book_keyword_api.py e refatora os testes deixando-os mais eficientes. concisos e fáceis de manter.

--- tests/api/test_book_keyword_api.py
+++ tests/api/test_book_keyword_api.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask.testing import FlaskClient
 from flask_jwt_extended import create_access_token
 from sqlalchemy import text
+from werkzeug.security import generate_password_hash
 
 from app import create_app
 from db import db
@@ -20,40 +21,57 @@ def app():
 
         db.session.execute(
             text(
-                f"""
-                INSERT INTO users (username, password, img_url)
-                    VALUES ('test', 'Senha@123', 'http://localhost:5000/users/keywords/test.jpg')
-                """
-            )
+                "INSERT INTO users (username, password, img_url) VALUES (:username, :password, :img_url)"
+            ),
+            {
+                'username': 'test',
+                'password': generate_password_hash('Senha@123'),
+                'img_url': 'http://localhost:5000/users/photos/test.jpg',
+            },
         )
 
-        db.session.execute(text("INSERT INTO book_genres (genre) VALUES ('fábula')"))
-        db.session.execute(text("INSERT INTO book_kinds (kind) VALUES ('físico')"))
+        db.session.execute(
+            text("INSERT INTO book_genres (genre) VALUES (:genre)"), {'genre': 'fábula'}
+        )
+        db.session.execute(text("INSERT INTO book_kinds (kind) VALUES (:kind)"), {'kind': 'físico'})
 
         db.session.execute(
             text(
-                """
+                """--sql
                 INSERT INTO books
                     (name, price, author, release_year, id_kind, id_genre)
-                    VALUES
-                        ('O Pequeno Príncipe', 10.99, 'Antoine de Saint-Exupéry', 1943, 1, 1),
-                        ('O Poderoso Chefão', 20.99, 'Mario Puzo', 1969, 1, 1)
+                    VALUES (:name, :price, :author, :release_year, :id_kind, :id_genre)
                 """
-            )
+            ),
+            [
+                {
+                    'name': 'O Pequeno Príncipe',
+                    'price': 10.99,
+                    'author': 'Antoine de Saint Exupéry',
+                    'release_year': 1943,
+                    'id_kind': 1,
+                    'id_genre': 1,
+                },
+                {
+                    'name': 'O Poderoso Chefão',
+                    'price': 20.99,
+                    'author': 'Mario Puzo',
+                    'release_year': 1969,
+                    'id_kind': 1,
+                    'id_genre': 1,
+                },
+            ],
         )
 
         db.session.execute(
-            text(
-                """
-                INSERT INTO book_keywords
-                    (keyword, id_book)
-                    VALUES
-                        ('dramático', 1),
-                        ('infantil', 1),
-                        ('máfia', 2)
-            """
-            )
+            text("INSERT INTO book_keywords (keyword, id_book) VALUES (:keyword, :id_book)"),
+            [
+                {'keyword': 'dramático', 'id_book': 1},
+                {'keyword': 'infantil', 'id_book': 1},
+                {'keyword': 'máfia', 'id_book': 2},
+            ],
         )
+
         db.session.commit()
 
     yield app
@@ -82,24 +100,28 @@ def test_add_book_keyword_in_lowercase(client: FlaskClient, access_token: str, a
     headers = {'Authorization': f'Bearer {access_token}'}
 
     book_id = 1
-
     data = {'keyword': 'emocionante'}
 
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=data)
     response_data = json.loads(response.data)
 
-    expected_data = {'error': False, 'status': 201, 'data': {'id': 4, 'keyword': 'emocionante'}}
+    new_keyword_id = 4
+    expected_data = {
+        'error': False,
+        'status': 201,
+        'data': {'id': new_keyword_id, 'keyword': data['keyword']},
+    }
 
     assert response_data == expected_data
     assert response.status_code == 201
 
     with app.app_context():
-        book_keyword = db.session.get(BookKeyword, 4)
+        book_keyword = db.session.get(BookKeyword, new_keyword_id)
 
         assert book_keyword is not None
-        assert book_keyword.id == 4
-        assert book_keyword.keyword == 'emocionante'
-        assert book_keyword.id_book == 1
+        assert book_keyword.id == new_keyword_id
+        assert book_keyword.keyword == data['keyword']
+        assert book_keyword.id_book == book_id
 
 
 def test_add_book_keyword_with_space_and_in_uppercase(
@@ -108,28 +130,28 @@ def test_add_book_keyword_with_space_and_in_uppercase(
     headers = {'Authorization': f'Bearer {access_token}'}
 
     book_id = 1
-
     data = {'keyword': ' MUITO EMOCIONANTE'}
 
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=data)
     response_data = json.loads(response.data)
 
+    new_keyword_id = 4
     expected_data = {
         'error': False,
         'status': 201,
-        'data': {'id': 4, 'keyword': 'muito emocionante'},
+        'data': {'id': new_keyword_id, 'keyword': data['keyword'].lower().strip()},
     }
 
     assert response_data == expected_data
     assert response.status_code == 201
 
     with app.app_context():
-        book_keyword = db.session.get(BookKeyword, 4)
+        book_keyword = db.session.get(BookKeyword, new_keyword_id)
 
         assert book_keyword is not None
-        assert book_keyword.id == 4
-        assert book_keyword.keyword == 'muito emocionante'
-        assert book_keyword.id_book == 1
+        assert book_keyword.id == new_keyword_id
+        assert book_keyword.keyword == data['keyword'].lower().strip()
+        assert book_keyword.id_book == book_id
 
 
 def test_when_try_to_add_book_keyword_with_invalid_data(client: FlaskClient, access_token: str):
@@ -142,9 +164,20 @@ def test_when_try_to_add_book_keyword_with_invalid_data(client: FlaskClient, acc
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=invalid_data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidDataSent'
-    assert response_data['status'] == 400
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidDataSent',
+        'message': [
+            {
+                'loc': ['keyword'],
+                'msg': 'String should have at least 3 characters',
+                'type': 'string_too_short',
+            }
+        ],
+        'status': 400,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 400
 
     invalid_data = {'keyword': '  '}
@@ -152,9 +185,20 @@ def test_when_try_to_add_book_keyword_with_invalid_data(client: FlaskClient, acc
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=invalid_data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidDataSent'
-    assert response_data['status'] == 400
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidDataSent',
+        'message': [
+            {
+                'loc': ['keyword'],
+                'msg': 'String should have at least 3 characters',
+                'type': 'string_too_short',
+            }
+        ],
+        'status': 400,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 400
 
     invalid_data = {'keyword': 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'}
@@ -162,9 +206,20 @@ def test_when_try_to_add_book_keyword_with_invalid_data(client: FlaskClient, acc
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=invalid_data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidDataSent'
-    assert response_data['status'] == 400
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidDataSent',
+        'message': [
+            {
+                'loc': ['keyword'],
+                'msg': 'String should have at most 20 characters',
+                'type': 'string_too_long',
+            }
+        ],
+        'status': 400,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 400
 
     invalid_data = {'keyword': 'adwa234@#$@#$as'}
@@ -172,9 +227,20 @@ def test_when_try_to_add_book_keyword_with_invalid_data(client: FlaskClient, acc
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=invalid_data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidDataSent'
-    assert response_data['status'] == 400
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidDataSent',
+        'message': [
+            {
+                'loc': ['keyword'],
+                'msg': "String should match pattern '^[a-zA-ZÀ-ÿç\\s\\d]+$'",
+                'type': 'string_pattern_mismatch',
+            }
+        ],
+        'status': 400,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 400
 
     invalid_data = {'keyword': 456}
@@ -182,9 +248,16 @@ def test_when_try_to_add_book_keyword_with_invalid_data(client: FlaskClient, acc
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=invalid_data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidDataSent'
-    assert response_data['status'] == 400
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidDataSent',
+        'message': [
+            {'loc': ['keyword'], 'msg': 'Input should be a valid string', 'type': 'string_type'}
+        ],
+        'status': 400,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 400
 
     invalid_data = {'keywords': 'teste'}
@@ -192,9 +265,21 @@ def test_when_try_to_add_book_keyword_with_invalid_data(client: FlaskClient, acc
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=invalid_data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidDataSent'
-    assert response_data['status'] == 400
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidDataSent',
+        'message': [
+            {'loc': ['keyword'], 'msg': 'Field required', 'type': 'missing'},
+            {
+                'loc': ['keywords'],
+                'msg': 'Extra inputs are not permitted',
+                'type': 'extra_forbidden',
+            },
+        ],
+        'status': 400,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 400
 
 
@@ -206,9 +291,14 @@ def test_when_try_to_add_book_keyword_without_data(client: FlaskClient, access_t
     response = client.post(f'/books/{book_id}/keywords', headers=headers)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'NoDataSent'
-    assert response_data['status'] == 400
+    expected_data = {
+        'error': True,
+        'error_name': 'NoDataSent',
+        'message': 'No data sent',
+        'status': 400,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 400
 
 
@@ -221,29 +311,37 @@ def test_when_try_to_add_book_keyword_with_content_type_multipart_form_data_retu
     }
 
     book_id = 1
-
     data = {'keyword': 'palavra'}
 
     response = client.post(f'/books/{book_id}/keywords', headers=headers, data=data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidContentType'
-    assert response_data['status'] == 415
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidContentType',
+        'message': 'Invalid Content-Type header',
+        'status': 415,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 415
 
 
 def test_when_try_to_add_book_keyword_without_auth_return_error_response(client: FlaskClient):
     book_id = 1
-
     data = {'keyword': 'palavra'}
 
     response = client.post(f'/books/{book_id}/keywords', json=data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'MissingJWT'
-    assert response_data['status'] == 401
+    expected_data = {
+        'error': True,
+        'error_name': 'MissingJWT',
+        'message': 'JWT token not provided',
+        'status': 401,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 401
 
 
@@ -251,15 +349,19 @@ def test_when_try_to_add_book_keyword_with_invalid_auth_return_error_response(cl
     headers = {'Authorization': f'Bearer 123'}
 
     book_id = 1
-
     data = {'keyword': 'palavra'}
 
     response = client.post(f'/books/{book_id}/keywords', headers=headers, json=data)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidJWT'
-    assert response_data['status'] == 401
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidJWT',
+        'message': 'Invalid JWT token',
+        'status': 401,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 401
 
 
@@ -278,7 +380,7 @@ def test_delete_book_keyword(client: FlaskClient, access_token: str, app: Flask)
     assert response.status_code == 200
 
     with app.app_context():
-        book_keyword = db.session.get(BookKeyword, 2)
+        book_keyword = db.session.get(BookKeyword, book_keyword_id)
 
         assert book_keyword is None
 
@@ -292,9 +394,14 @@ def test_when_try_to_delete_book_keyword_without_auth_return_error_response(clie
     response = client.delete(f'/books/{book_id}/keywords/{book_keyword_id}', headers=headers)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'MissingJWT'
-    assert response_data['status'] == 401
+    expected_data = {
+        'error': True,
+        'error_name': 'MissingJWT',
+        'message': 'JWT token not provided',
+        'status': 401,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 401
 
 
@@ -312,9 +419,14 @@ def test_when_try_to_delete_book_keyword_with_invalid_auth_return_error_response
     response = client.delete(f'/books/{book_id}/keywords/{book_keyword_id}', headers=headers)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'InvalidJWT'
-    assert response_data['status'] == 401
+    expected_data = {
+        'error': True,
+        'error_name': 'InvalidJWT',
+        'message': 'Invalid JWT token',
+        'status': 401,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 401
 
 
@@ -329,9 +441,14 @@ def test_when_try_to_delete_last_book_keyword_returns_error_response(
     response = client.delete(f'/books/{book_id}/keywords/{book_keyword_id}', headers=headers)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'BookMustHaveAtLeastOneKeyword'
-    assert response_data['status'] == 400
+    expected_data = {
+        'error': True,
+        'error_name': 'BookMustHaveAtLeastOneKeyword',
+        'message': f'The book {book_id} must have at least one keyword',
+        'status': 400,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 400
 
 
@@ -346,9 +463,14 @@ def test_when_try_to_delete_book_keyword_does_not_exists_returns_error_response(
     response = client.delete(f'/books/{book_id}/keywords/{book_keyword_id}', headers=headers)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'BookKeywordDoesntExists'
-    assert response_data['status'] == 404
+    expected_data = {
+        'error': True,
+        'error_name': 'BookKeywordDoesntExists',
+        'message': f'The keyword {book_keyword_id} does not exist',
+        'status': 404,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 404
 
 
@@ -363,9 +485,14 @@ def test_when_try_to_delete_book_keyword_from_book_does_not_exists_returns_error
     response = client.delete(f'/books/{book_id}/keywords/{book_keyword_id}', headers=headers)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'BookDoesntExists'
-    assert response_data['status'] == 404
+    expected_data = {
+        'error': True,
+        'error_name': 'BookDoesntExists',
+        'message': f'The book {book_id} does not exist',
+        'status': 404,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 404
 
 
@@ -380,7 +507,12 @@ def test_when_try_to_delete_book_keyword_from_book_does_not_own_it_returns_error
     response = client.delete(f'/books/{book_id}/keywords/{book_keyword_id}', headers=headers)
     response_data = json.loads(response.data)
 
-    assert response_data['error']
-    assert response_data['error_name'] == 'BookDoesntOwnThisKeyword'
-    assert response_data['status'] == 401
+    expected_data = {
+        'error': True,
+        'error_name': 'BookDoesntOwnThisKeyword',
+        'message': f'The keyword {book_keyword_id} does not belong to the book {book_id}',
+        'status': 401,
+    }
+
+    assert response_data == expected_data
     assert response.status_code == 401
