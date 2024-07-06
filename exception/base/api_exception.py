65798d16b1e09aff6b8a5b68ca64caee16f959bb
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-06 15:07:11 -0300

    refactor: Alterar classe ApiException
    Altera as seguintes coisas na classe:
    
    - Altera os parâmetros recebidos pelo método __init__
    - Torna os atributos privados
    - Adiciona os atributos: _scope, _detail (opcional) e timestamp
    - Adiciona o método serialize
    - Adiciona a @property status

--- exception/base/api_exception.py
+++ exception/base/api_exception.py
@@ -1,10 +1,32 @@
+from datetime import datetime, timezone
 from typing import Any
 
 
 class ApiException(Exception):
-    def __init__(self, *, message: Any, status: int) -> None:
-        self.name = self.__class__.__name__
-        self.message = message
-        self.status = status
+    def __init__(self, *, message: str, status: int, detail: Any | None = None) -> None:
+        self._name = self.__class__.__name__
+        self._message = message
+        self._status = status
+        self._scope = self.__class__.__qualname__.split('.')[0]
+        self._detail = detail
+        self._timestamp = datetime.now(timezone.utc).isoformat()
 
         super().__init__()
+
+    def searialize(self) -> dict[str, Any]:
+        serialization = {
+            'code': self._name,
+            'scope': self._scope,
+            'message': self._message,
+            'status': self._status,
+            'timestamp': self._timestamp,
+        }
+
+        if self._detail is not None:
+            serialization['detail'] = self._detail
+
+        return serialization
+
+    @property
+    def status(self) -> int:
+        return self._status
