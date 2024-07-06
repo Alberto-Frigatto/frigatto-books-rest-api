7a79de323495e8c2e5b5c936aacb4fdfe07532d5
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-06 15:19:16 -0300

    refactor: Alterar método json da classe PaginationResponse

--- utils/response/pagination_response.py
+++ utils/response/pagination_response.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import flask
+from flask import request, url_for
 from flask_sqlalchemy.pagination import Pagination
 
 from .base import Response
@@ -12,23 +13,29 @@ class PaginationResponse(Response):
         self._data = data
 
     def json(self) -> flask.Response:
-        status = 200
-        total_items = (
-            self._pagination_details.total if self._pagination_details.total is not None else 0
+        total_items = self._pagination_details.total or 0
+        actual_page = self._pagination_details.page or 1
+        prev_page = (
+            url_for(request.endpoint, page=self._pagination_details.prev_num)
+            if self._pagination_details.has_prev and request.endpoint
+            else None
+        )
+        next_page = (
+            url_for(request.endpoint, page=self._pagination_details.next_num)
+            if self._pagination_details.has_next and request.endpoint
+            else None
         )
 
         response = {
-            'error': False,
-            'status': status,
             'data': self._data,
             'total_items': total_items,
             'total_pages': self._pagination_details.pages,
-            'page': self._pagination_details.page,
+            'page': actual_page,
             'per_page': self._pagination_details.per_page,
             'has_prev': self._pagination_details.has_prev,
             'has_next': self._pagination_details.has_next,
-            'prev_page': self._pagination_details.prev_num,
-            'next_page': self._pagination_details.next_num,
+            'prev_page': prev_page,
+            'next_page': next_page,
         }
 
-        return super()._make_response(response, status)
+        return super()._make_response(payload=response, status=200)
