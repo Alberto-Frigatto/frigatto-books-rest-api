bb8d9b6d79b2697093d48947431d38049078a835
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-16 14:53:42 -0300

    chore: Adicionar import à create_upload_dirs_if_dont_exist no pacote config

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,5 @@
 from .di import di_config
 from .error_handler import add_error_handlers
+from .init_setup import create_upload_dirs_if_dont_exist
 from .middleware import add_middlewares
 from .routes import add_routes
