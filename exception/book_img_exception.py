de0080268fe31397d31e55cf1c7be9346ea87de1
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-25 12:05:34 -0300

    refactor: Alterar exceções de BookImgException
    Renomeia a exceção BookImgDoesntExists para BookImgDoesntExist. Altera o status da exceção BookDoesntOwnThisImg e altera a mensagem da exceção BookAlreadyHaveImageMaxQty.

--- exception/book_img_exception.py
+++ exception/book_img_exception.py
@@ -2,7 +2,7 @@ from .base import ApiException
 
 
 class BookImgException:
-    class BookImgDoesntExists(ApiException):
+    class BookImgDoesntExist(ApiException):
         def __init__(self, id: str) -> None:
             super().__init__(
                 message=f'The image {id} does not exist',
@@ -13,7 +13,7 @@ class BookImgException:
         def __init__(self, id_img: str, id_book: str) -> None:
             super().__init__(
                 message=f'The image {id_img} does not belong to the book {id_book}',
-                status=401,
+                status=403,
             )
 
     class BookMustHaveAtLeastOneImg(ApiException):
@@ -26,6 +26,6 @@ class BookImgException:
     class BookAlreadyHaveImageMaxQty(ApiException):
         def __init__(self, name: str) -> None:
             super().__init__(
-                message=f'The book "{name}" already have the max quantity of images',
+                message=f'The book "{name}" already has the max quantity of images',
                 status=400,
             )
