b5946e4bee170f82df002ae3f321a24dbcde46cc
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-07-25 12:03:05 -0300

    chore: Remover header Content-Type de app_general.py

--- config/app_general.py
+++ config/app_general.py
@@ -5,7 +5,6 @@ JWT_ACCESS_TOKEN_EXPIRES = False
 JWT_COOKIE_SECURE = True
 JWT_COOKIE_SAMESITE = "Strict"
 RESPONSE_HEADERS = [
-    ('Content-Type', 'application/json;charset=utf-8'),
     ('Access-Control-Allow-Origin', os.getenv('ALLOW-ORIGIN', 'http://127.0.0.1:5500')),
     ('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-CSRF-TOKEN'),
     ('Access-Control-Allow-Credentials', 'true'),
